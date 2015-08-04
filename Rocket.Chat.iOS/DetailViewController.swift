71511a58e723acba10b0686c1314bef775740aca
Yorgos Saslis <g.saslis@gmail.com>
2015-08-04 12:54:40 +0300

    Initial import with the sample master-view detail app from xcode
    Have replaced the default addButton with a simple < button that leads to an empty view.

---
+++ Rocket.Chat.iOS/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Rocket.Chat.iOS
+//
+//  Created by giorgos on 8/4/15.
+//  Copyright © 2015 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.valueForKey("timeStamp")!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
