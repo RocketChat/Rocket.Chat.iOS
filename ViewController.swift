776252cc2b0074425b509f5abf1dcbfc2434f38c
Kornelakis Michael <komic@kornelakiss-macbook-pro.local>
2015-08-10 15:00:44 +0300

    added a general avatar image populate data in table adding missing constraints in prototype cell

--- ViewController.swift
+++ ViewController.swift
@@ -19,14 +19,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Do any additional setup after loading the view, typically from a nib.
         
         
-        let u1:User = User(id: "1", username: "Komic", avatar: "komic.png", status: User.Status.ONLINE, timezone: NSTimeZone.systemTimeZone())
+        let u1:User = User(id: "1", username: "Komic", avatar: "avatar.png", status: User.Status.ONLINE, timezone: NSTimeZone.systemTimeZone())
         
         let msg1:Message = Message(id: "1", text: "Message 1 from u1", tstamp: NSDate(), user: u1)
         
-        let u2:User = User(id: "2", username: "Yorgos", avatar: "yorgos.png", status: User.Status.ONLINE, timezone: NSTimeZone.systemTimeZone())
+        let u2:User = User(id: "2", username: "Yorgos", avatar: "avatar.png", status: User.Status.ONLINE, timezone: NSTimeZone.systemTimeZone())
         let msg2:Message = Message(id: "2", text: "Message 2 from u2", tstamp: NSDate(), user: u2)
         
-        let u3:User = User(id: "3", username: "GeorgeP", avatar: "georgeP.png", status: User.Status.ONLINE, timezone: NSTimeZone.systemTimeZone())
+        let u3:User = User(id: "3", username: "GeorgeP", avatar: "avatar.png", status: User.Status.ONLINE, timezone: NSTimeZone.systemTimeZone())
         let msg3:Message = Message(id: "3", text: "Message 3 from u3", tstamp: NSDate(), user: u3)
         
         let uSet1:Set<User> = [u1, u2, u3]
@@ -79,9 +79,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        var cell:MainTableViewCell = mainTableview.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! MainTableViewCell
+        let cell:MainTableViewCell = mainTableview.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! MainTableViewCell
         
+        print("\(cR1!.messages[indexPath.row].user)")
         print("\(cR1?.messages[indexPath.row].text)")
+        print("\(cR1?.messages[indexPath.row].tstamp)")
+        cell.avatarImg.image = UIImage(named: "avatar.png")
+        cell.usernameLabel.text = "\(cR1!.messages[indexPath.row].user.username)"
+        cell.timeLabel.text = "\(cR1!.messages[indexPath.row].tstamp)"
         cell.messageLabel.text = "\(cR1!.messages[indexPath.row].text)"
         
         return cell
