6e75c274c62ab73a1fc99ccd788e2b451b3dc625
George Peponakis <peponakis@cytech.gr>
2015-08-18 16:55:59 +0300

    Add CoreData model and sample code for User

--- Rocket.Chat.iOS-Bridging-Header.h
+++ Rocket.Chat.iOS-Bridging-Header.h
@@ -1,5 +1,13 @@
 //
-//  Use this file to import your target's public headers that you would like to expose to Swift.
+//  Rocket.Chat.iOS-Bridging-Header.h
+//  Rocket.Chat.iOS
+//
+//  Created by Mobile Apps on 8/17/15.
+//  Copyright © 2015 Rocket.Chat. All rights reserved.
 //
 
-#import "MMDrawerController.h"
+#ifndef Rocket_Chat_iOS_Bridging_Header_h
+#define Rocket_Chat_iOS_Bridging_Header_h
+
+
+#endif /* Rocket_Chat_iOS_Bridging_Header_h */
