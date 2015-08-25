dec0781eedfa69993db155cf54da9abb78b62cc6
Kornelakis Michael <michayc@yahoo.com>
2015-08-26 00:29:21 +0300

    no message

---
+++ Rocket.Chat.iOS/ViewControllers/LeftViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LeftViewController.swift
+//  Rocket.Chat.iOS
+//
+//  Created by Kornelakis Michael on 8/6/15.
+//  Copyright © 2015 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+
+class LeftViewController: UIViewController {
+
+
+}
