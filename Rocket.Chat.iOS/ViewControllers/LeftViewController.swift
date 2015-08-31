f2347812c9566cafb610fd30ac307ccb85293db7
Kornelakis Michael <michayc@yahoo.com>
2015-09-01 00:34:04 +0300

    Revert "Merge branch 'develop' of https://github.com/gsaslis/Rocket.Chat.iOS into develop"
    This reverts commit c451d480e02ecad8987c27e6cb4e2f08f935a52c, reversing
    changes made to 462b84a3b26108bbb7d755daff016daf35fc379a.

--- Rocket.Chat.iOS/ViewControllers/LeftViewController.swift
+++ Rocket.Chat.iOS/ViewControllers/LeftViewController.swift
@@ -8,54 +8,7 @@
 
 import UIKit
 
-class LeftViewController: UIViewController, SwitchAccountViewDelegate {
-
-  @IBOutlet weak var accountTopView: UIView!
-  @IBOutlet weak var leftMainContainer: UIView!
-  
-  var tabBarContainer : LeftMenuTabBarViewController?
-  
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-      
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-//  override func prefersStatusBarHidden() -> Bool {
-//    return true
-//  }
-//  
-
-  // MARK: - Navigation
-  
-  // In a storyboard-based application, you will often want to do a little preparation before navigation
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    
-    //set self as the delegate in the account bar that will handle the event coming up from there.
-    if segue.identifier == "embedAccountBar"{
-      let vc = segue.destinationViewController as! AccountBarViewController
-      vc.delegate = self
-    } else if segue.identifier == "embedChatNav"{
-      tabBarContainer = segue.destinationViewController as? LeftMenuTabBarViewController
-    }
-  }
-  
-  // MARK: SwitchAccountViewDelegate
-  func didClickOnAccountBar(accountOptionsWereOpen: Bool){
-		// not much to do here, simply let the tab bar container know about the event, so it can swap the selected view.
-    if (tabBarContainer != nil){
-      tabBarContainer?.didClickOnAccountBar(accountOptionsWereOpen)
-    }
-  }
-
+class LeftViewController: UIViewController {
 
 
 }
