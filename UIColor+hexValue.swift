e947175fe1f8d65f4f56d3759ec25e2511f7b6ab
Kornelakis Michael <michayc@yahoo.com>
2015-09-09 16:38:52 +0300

    Set colours (Need to be checked) Also added func to return the darker blue which is used as a background for the account bar

--- UIColor+hexValue.swift
+++ UIColor+hexValue.swift
@@ -19,25 +19,32 @@ extension UIColor {
 
     static func rocketMainFontColor() -> UIColor {
         
-        return UIColor(red: 255.0, green: 255.0, blue: 255.0, alpha: 1)
+        return UIColor(red: 68/255.0, green: 68/255.0, blue: 68/255.0, alpha: 1)
         
     }
     
     static func rocketSecondaryFontColor() -> UIColor {
         
-        return UIColor(red: 255.0, green: 255.0, blue: 255.0, alpha: 1)
+        return UIColor(red: 127/255.0, green: 127/255.0, blue: 127/255.0, alpha: 1)
         
     }
     
     static func rocketRedColor() -> UIColor {
         
-        return UIColor(red: 255.0, green: 255.0, blue: 255.0, alpha: 1)
+        return UIColor(red: 193/255.0, green: 38/255.0, blue: 35/255.0, alpha: 1)
         
     }
     
     static func rocketBlueColor() -> UIColor {
         
-        return UIColor(red: 255.0, green: 255.0, blue: 255.0, alpha: 1)
+        return UIColor(red: 4/255.0, green: 73/255.0, blue: 116/255.0, alpha: 1)
+        
+    }
+    
+    static func rocketDarkBlueColor() -> UIColor {
+        
+        return UIColor(red: 4/255.0, green: 67/255.0, blue: 106/255.0, alpha: 1)
+
         
     }
     
