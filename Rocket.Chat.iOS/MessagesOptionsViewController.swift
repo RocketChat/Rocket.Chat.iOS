cb31cbd47c151dd4958b01d8aa4ff8a156194635
Kornelakis Michael <michayc@yahoo.com>
2015-09-18 21:45:57 +0300

    Added labels and switches for the preferences screen

--- Rocket.Chat.iOS/MessagesOptionsViewController.swift
+++ Rocket.Chat.iOS/MessagesOptionsViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class MessagesOptionsViewController: UIViewController {
 
+    
+    //Labels
     @IBOutlet var useEmojisLabel: UILabel!
     @IBOutlet var convertAsciiToEmojiLabel: UILabel!
     @IBOutlet var autoLoadImagesLabel: UILabel!
     @IBOutlet var saveMobileBandWidthLabel: UILabel!
+    
+    
+    //Switches
+    @IBOutlet var useEmojisSwitch: UISwitch!
+    @IBOutlet var convertAsciiToEmojiSwitch: UISwitch!
+    @IBOutlet var autoLoadImagesSwitch: UISwitch!
+    @IBOutlet var saveMobileBandwidthSwitch: UISwitch!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,5 +44,67 @@ class MessagesOptionsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    //MARK: Messages Options Actions
+    
+    @IBAction func useEmojisAction(sender: AnyObject) {
+    
+        if useEmojisSwitch.on {
+            
+            print("Use emojis is ON")
+            
+        } else {
+            
+            print("Use emojis is OFF")
+            
+        }
+        
+        
+    }
 
+    
+    @IBAction func convertAsciiToEmojiAction(sender: AnyObject) {
+        
+        if convertAsciiToEmojiSwitch.on {
+            
+            print("Convert Ascii to Emoji is ON")
+            
+        } else {
+            
+            print("Convert Ascii to Emoji is OFF")
+            
+        }
+        
+    }
+    
+    @IBAction func autoLoadImagesAction(sender: AnyObject) {
+        
+        if autoLoadImagesSwitch.on {
+            
+            print("Auto Load Images is ON")
+            
+        } else {
+            
+            print("Auto Load Images is OFF")
+            
+        }
+
+        
+    }
+    
+    @IBAction func saveMobileBandwidthAction(sender: AnyObject) {
+        
+        if saveMobileBandwidthSwitch.on {
+            
+            print("Save Mobile Bandwidth is ON")
+            
+        } else {
+            
+            print("Save Mobile Bandwidth is OFF")
+            
+        }
+
+        
+    }
+    
 }
