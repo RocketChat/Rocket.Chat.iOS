72b49ecc33a6789d129fba765084de73d83bb160
Yorgos Saslis <g.saslis@gmail.com>
2015-09-24 10:49:11 +0300

    Added meteorClient to AppDelegate
    According to the swiftExample in the ObjectiveDDP [github](https://github.com/boundsj/ObjectiveDDP) project, we need to create the meteorClient centrally.
    Then we can e.g. pass references to all classes who need to make use of it, or find some other way to share this common resource between all instances who need it.

--- Rocket.Chat.iOS/AppDelegate.swift
+++ Rocket.Chat.iOS/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import JSQCoreDataKit
 import MMDrawerController
+import ObjectiveDDP
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,11 +20,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var centerContainer : MMDrawerController?
     private(set) var model : CoreDataModel?
     private(set) var stack : CoreDataStack?
+
+    var meteorClient = initialiseMeteor("pre2", "ws://localhost:4000/websocket");
+
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         model = CoreDataModel(name: "Rocket_Chat_iOS")
         stack = CoreDataStack(model: model!)
 
+//      let storyboard = UIStoryboard(name: "Main", bundle: nil)
+
+//        let loginController:LoginViewController =  storyboard.instantiateViewControllerWithIdentifier("loginViewController") as! LoginViewController
+//        loginController.meteor = self.meteorClient
+
+//        meteorClient.addSubscription("things")
+//        meteorClient.addSubscription("lists")
+
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reportConnection", name: MeteorClientDidConnectNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reportDisconnection", name:MeteorClientDidDisconnectNotification, object: nil)
+
         return true
     }
+
+        func reportConnection() {
+            print("================> connected to server!")
+        }
+
+        func reportDisconnection() {
+            print("================> disconnected from server!")
+        }
+
+
 }
