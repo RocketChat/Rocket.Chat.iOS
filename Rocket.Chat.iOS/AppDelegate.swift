61d3ab7444498a7a979574b7efa8010c8241e398
Yorgos Saslis <g.saslis@gmail.com>
2015-09-25 01:00:11 +0300

    Added observer for `websocketReady` and fixed indentation

--- Rocket.Chat.iOS/AppDelegate.swift
+++ Rocket.Chat.iOS/AppDelegate.swift
@@ -13,42 +13,56 @@ import ObjectiveDDP
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  // Variable to keep the MMDrawerController instance
+  var centerContainer : MMDrawerController?
+  private(set) var model : CoreDataModel?
+  private(set) var stack : CoreDataStack?
+  
+  var meteorClient = initialiseMeteor("pre2", "ws://localhost:4000/websocket");
+  
+  
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    model = CoreDataModel(name: "Rocket_Chat_iOS")
+    stack = CoreDataStack(model: model!)
     
-    var window: UIWindow?
+    let observingOption = NSKeyValueObservingOptions.New
+    meteorClient.addObserver(self, forKeyPath:"websocketReady", options: observingOption, context:nil)
     
-    // Variable to keep the MMDrawerController instance
-    var centerContainer : MMDrawerController?
-    private(set) var model : CoreDataModel?
-    private(set) var stack : CoreDataStack?
-
-    var meteorClient = initialiseMeteor("pre2", "ws://localhost:4000/websocket");
-
     
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        model = CoreDataModel(name: "Rocket_Chat_iOS")
-        stack = CoreDataStack(model: model!)
-
-//      let storyboard = UIStoryboard(name: "Main", bundle: nil)
-
-//        let loginController:LoginViewController =  storyboard.instantiateViewControllerWithIdentifier("loginViewController") as! LoginViewController
-//        loginController.meteor = self.meteorClient
-
-//        meteorClient.addSubscription("things")
-//        meteorClient.addSubscription("lists")
-
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reportConnection", name: MeteorClientDidConnectNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reportDisconnection", name:MeteorClientDidDisconnectNotification, object: nil)
-
-        return true
+    //      let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    
+    //        let loginController:LoginViewController =  storyboard.instantiateViewControllerWithIdentifier("loginViewController") as! LoginViewController
+    //        loginController.meteor = self.meteorClient
+    
+    //        meteorClient.addSubscription("things")
+    //        meteorClient.addSubscription("lists")
+    
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: "reportConnection", name: MeteorClientDidConnectNotification, object: nil)
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: "reportDisconnection", name:MeteorClientDidDisconnectNotification, object: nil)
+    
+    return true
+  }
+  
+  func reportConnection() {
+    print("================> connected to server!")
+  }
+  
+  func reportDisconnection() {
+    print("================> disconnected from server!")
+  }
+  
+  // MARK: - Connection
+  override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<(Void)>) {
+    
+    if (keyPath == "websocketReady" && meteorClient.websocketReady) {
+      //      connectionStatusText.text = "Connected to Todo Server"
+      //      var image:UIImage = UIImage(named: "green_light.png")!
+      //      connectionStatusLight.image = image
+      print("connected to server!!!")
     }
-
-        func reportConnection() {
-            print("================> connected to server!")
-        }
-
-        func reportDisconnection() {
-            print("================> disconnected from server!")
-        }
-
-
+  }
+  
 }
