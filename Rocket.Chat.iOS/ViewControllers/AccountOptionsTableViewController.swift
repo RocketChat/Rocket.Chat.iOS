b8515778fda460fe3fc477edaf88b85429bcd398
Kornelakis Michael <michayc@yahoo.com>
2015-11-18 00:12:45 +0200

    added case where user chooses a status and calling meteor methods to change the status when the user selects one. deleted code when user selects MySettings and replaced with a call to the chooseOptionAndSetViews function

--- Rocket.Chat.iOS/ViewControllers/AccountOptionsTableViewController.swift
+++ Rocket.Chat.iOS/ViewControllers/AccountOptionsTableViewController.swift
@@ -62,59 +62,71 @@ class AccountOptionsTableViewController: UITableViewController {
     //Here is what happens when the user select's an option from account options
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
     
-        
+        //User's Status
+        if (indexPath.section == 0){
+            //Get the username and
+            
+            switch indexPath.row {
+                
+            case 0:
+                print("online")
+                self.meteor.callMethodName("UserPresence:setDefaultStatus", parameters: ["online"], responseCallback: { (response, error) -> Void in
+                    
+                    if error != nil{
+                        print("Error: \(error.description)")
+                        return
+                    }
+                    
+//                    print(response["result"])
+                    self.chooseOptionAndSetViews("ChatNav",color: "Green")
+                })
+            case 1:
+                print("Away")
+                self.meteor.callMethodName("UserPresence:setDefaultStatus", parameters: ["away"], responseCallback: { (response, error) -> Void in
+                    
+                    if error != nil{
+                        print("Error: \(error.description)")
+                        return
+                    }
+                    
+//                    print(response["result"])
+                    self.chooseOptionAndSetViews("ChatNav",color: "Yellow")
+                })
+            case 2:
+                print("Busy")
+                self.meteor.callMethodName("UserPresence:setDefaultStatus", parameters: ["busy"], responseCallback: { (response, error) -> Void in
+                    
+                    if error != nil{
+                        print("Error: \(error.description)")
+                        return
+                    }
+                    
+//                    print(response["result"])
+                    self.chooseOptionAndSetViews("ChatNav",color: "Red")
+                })
+            case 3:
+                print("Invisible")
+                
+                self.meteor.callMethodName("UserPresence:setDefaultStatus", parameters: ["offline"], responseCallback: { (response, error) -> Void in
+                    
+                    if error != nil{
+                        print("Error: \(error.description)")
+                        return
+                    }
+                    
+//                    print(response["result"])
+                    self.chooseOptionAndSetViews("ChatNav",color:"Grey")
+                })
+            default:
+                print("default")
+            }
+            
+        }
         //If user selects MySettings
-        if (indexPath.section == 1 && indexPath.row == 0) {
+        else if (indexPath.section == 1 && indexPath.row == 0) {
          
-            //get the appDelegate
-            let appdelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-            
-            
-            //Create MySettingsViewController instance
-            let mySettingsVC = storyboard?.instantiateViewControllerWithIdentifier("mySettings") as! MySettingsViewController
-            
-            //Set it as rootViewController in the navigation controller
-            let centerNewNav = UINavigationController(rootViewController: mySettingsVC)
-            
-            
-            //Set the settings controller as the center view controller in the MMDrawer
-            appdelegate.centerContainer?.setCenterViewController(centerNewNav, withCloseAnimation: false, completion: nil)
-            
-            //Close the drawer
-            appdelegate.centerContainer?.closeDrawerAnimated(true, completion: nil)
-            
-            
-            //Get the AccountBar's tab controller
-            let MyAccountTabBarController = tabBarController?.parentViewController?.childViewControllers[0] as! MyAccountBarTabViewController
-            
+            self.chooseOptionAndSetViews("Settings", color: nil)
 
-            //Set the account bar view
-            MyAccountTabBarController.selectedViewController = MyAccountTabBarController.viewControllers![1]
-            
-            
-            
-            /******** Prepare the account bar for when we exit settings ********/
-            
-            //Get the accountBarView
-            let accountBarView = MyAccountTabBarController.viewControllers![MyAccountTabBarController.findIndexOfAccountBar()] as! AccountBarViewController
-            
-            //Set accountOptionWereOpen to false so when we get back from settings the account bar will work right
-            accountBarView.accountOptionsWereOpen = false
-            
-            //Rotate the arrow down
-            accountBarView.detailsButton?.imageView?.image = UIImage(named: "Arrow-Down")
-            
-            /************/
-            
-            
-            
-            //get LeftMenu's tab bar controller
-            let leftMenuTabBarController = tabBarController as! LeftMenuTabBarViewController
-            
-            //Set the left menu's view
-            tabBarController?.selectedViewController = tabBarController?.viewControllers![leftMenuTabBarController.findIndexOfMySettings()]
-            
-            
         }
     
         else if (indexPath.section == 1 && indexPath.row == 1) {
