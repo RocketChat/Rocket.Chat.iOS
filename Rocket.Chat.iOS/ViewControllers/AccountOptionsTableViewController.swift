b8b2a7408c0b6685142b05d1113ce71e79139fce
Kornelakis Michael <michayc@yahoo.com>
2015-11-18 00:09:38 +0200

    added func chooseOptionsAndSetViews to handle left menu's view when the user chooses an option and set the statusIcon when the user changes status. Also turn the arrow down at the account bar

--- Rocket.Chat.iOS/ViewControllers/AccountOptionsTableViewController.swift
+++ Rocket.Chat.iOS/ViewControllers/AccountOptionsTableViewController.swift
@@ -126,28 +126,80 @@ class AccountOptionsTableViewController: UITableViewController {
     
             if (meteor.connected){
                 
-//                meteor.callMethodName("leaveRoom", parameters: ["GENERAL"], responseCallback: { (response, error) -> Void in
-//                    
-//                    if error != nil {
-//                        print(error.description)
-//                    }else{
-//                        print(response)
-//                    }
-//                    
-//                })
-                
                 meteor.logout()
                 print("Logged out")
                 ad.window?.rootViewController = loginVC
+            
             } else {
+            
                 let alert = UIAlertController(title: "Error", message: "Connection is lost", preferredStyle: UIAlertControllerStyle.Alert)
                 let cancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
                 alert.addAction(cancel)
                 self.presentViewController(alert, animated: true, completion: nil)
+            
             }
             
         }
     
 
+    }
+    
+    
+    /* 
+    This func is to handle left menu's view when user chooses an option.
+    Also when user chooses a status the statusIcon changes color and
+    the arrow turns down
+    */
+    func chooseOptionAndSetViews(viewToAppear:String,color:String?) {
+        
+        //Get the AccountBar's tab controller
+        let MyAccountTabBarController = tabBarController?.parentViewController?.childViewControllers[0] as! MyAccountBarTabViewController
+        
+        /******** Prepare the account bar for when we exit settings ********/
+         
+         //Get the accountBarView
+        let accountBarView = MyAccountTabBarController.viewControllers![MyAccountTabBarController.findIndexOfAccountBar()] as! AccountBarViewController
+        
+        //Set accountOptionWereOpen to false so when we get back from settings the account bar will work right
+        accountBarView.accountOptionsWereOpen = false
+        
+        //Rotate the arrow down
+        accountBarView.detailsButton?.imageView?.image = UIImage(named: "Arrow-Down")
+        
+        if color != nil {
+            accountBarView.statusIcon.image = UIImage(named: color!)
+        }
+        /************/
+         
+         //get LeftMenu's tab bar controller
+        let leftMenuTabBarController = tabBarController as! LeftMenuTabBarViewController
+        
+        
+        switch viewToAppear{
+            
+            case "ChatNav":
+                //Set the left menu's view
+                tabBarController?.selectedViewController = tabBarController?.viewControllers![leftMenuTabBarController.findIndexOfChatNav()]
+                
+            case "Settings":
+                //Create MySettingsViewController instance
+                let mySettingsVC = storyboard?.instantiateViewControllerWithIdentifier("mySettings") as! MySettingsViewController
+                
+                //Set it as rootViewController in the navigation controller
+                let centerNewNav = UINavigationController(rootViewController: mySettingsVC)
+                
+                
+                //Set the settings controller as the center view controller in the MMDrawer
+                self.ad!.centerContainer?.setCenterViewController(centerNewNav, withCloseAnimation: false, completion: nil)
+                
+                //Close the drawer
+                self.ad!.centerContainer?.closeDrawerAnimated(true, completion: nil)
+                tabBarController?.selectedViewController = tabBarController?.viewControllers![leftMenuTabBarController.findIndexOfMySettings()]
+            
+            default:
+                tabBarController?.selectedViewController = tabBarController?.viewControllers![leftMenuTabBarController.findIndexOfMySettings()]
+        
+        }
+        
     }
 }
