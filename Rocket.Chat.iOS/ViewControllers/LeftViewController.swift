724356765d210df3f8034b0a7c6bfa71ed964e82
Kornelakis Michael <michayc@yahoo.com>
2015-12-06 17:06:33 +0200

    Added the SwitchRoomDelegate. Added a variable to get the current ViewController. When the ChatsNavTableViewController is visible on the left menu we set the ChatsNaTableViewController's delegate so it can be used there when a room is selected. Added didSelectRoom function of the SwitchRoomViewDelegate. When we select a room the ChatsNavTableViewController notifies the delegate and this didSelectRoom function notifies the ViewController
    Also some reformatting the code

--- Rocket.Chat.iOS/ViewControllers/LeftViewController.swift
+++ Rocket.Chat.iOS/ViewControllers/LeftViewController.swift
@@ -8,62 +8,72 @@
 
 import UIKit
 
-class LeftViewController: UIViewController, SwitchAccountViewDelegate {
-
-  @IBOutlet weak var accountTopView: UIView!
-  @IBOutlet weak var leftMainContainer: UIView!
-  
+class LeftViewController: UIViewController, SwitchAccountViewDelegate, SwitchRoomDelegate {
+    
+    @IBOutlet weak var accountTopView: UIView!
+    @IBOutlet weak var leftMainContainer: UIView!
+    
+    var viewController: ViewController?
     var accountTabBarContainer : MyAccountBarTabViewController?
     var tabBarContainer : LeftMenuTabBarViewController?
-  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         self.view.backgroundColor = UIColor.rocketBlueColor()
-
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-  override func prefersStatusBarHidden() -> Bool {
-    switch UIDevice.currentDevice().orientation{
-    case .Portrait, .PortraitUpsideDown:
-			return false
-    case .LandscapeLeft, .LandscapeRight:
-    	return true
-    default:
-      return false
-    }
-
-  }
-  
-
-  // MARK: - Navigation
-  
-  // In a storyboard-based application, you will often want to do a little preparation before navigation
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     
-    //set self as the delegate in the account bar that will handle the event coming up from there.
-    if segue.identifier == "embedAccountBar"{
-      accountTabBarContainer = segue.destinationViewController as? MyAccountBarTabViewController
-        let accountBarTabView = accountTabBarContainer?.viewControllers![0] as! AccountBarViewController
-        accountBarTabView.delegate = self
-    } else if segue.identifier == "embedChatNav"{
-      tabBarContainer = segue.destinationViewController as? LeftMenuTabBarViewController
+    override func prefersStatusBarHidden() -> Bool {
+        switch UIDevice.currentDevice().orientation{
+        case .Portrait, .PortraitUpsideDown:
+            return false
+        case .LandscapeLeft, .LandscapeRight:
+            return true
+        default:
+            return false
+        }
+        
     }
-  }
-  
-  // MARK: SwitchAccountViewDelegate
-  func didClickOnAccountBar(accountOptionsWereOpen: Bool){
-		// not much to do here, simply let the tab bar container know about the event, so it can swap the selected view.
-    if (tabBarContainer != nil){
-      tabBarContainer?.didClickOnAccountBar(accountOptionsWereOpen)
+    
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        //set self as the delegate in the account bar that will handle the event coming up from there.
+        if segue.identifier == "embedAccountBar"{
+            accountTabBarContainer = segue.destinationViewController as? MyAccountBarTabViewController
+            let accountBarTabView = accountTabBarContainer?.viewControllers![0] as! AccountBarViewController
+            accountBarTabView.delegate = self
+        } else if segue.identifier == "embedChatNav"{
+            tabBarContainer = segue.destinationViewController as? LeftMenuTabBarViewController
+            let chatsNavTableView = tabBarContainer?.viewControllers![0] as! ChatsNavTableViewController
+            chatsNavTableView.delegate = self
+        }
     }
-  }
-
+    
+    // MARK: SwitchAccountViewDelegate
+    func didClickOnAccountBar(accountOptionsWereOpen: Bool){
+        // not much to do here, simply let the tab bar container know about the event, so it can swap the selected view.
+        if (tabBarContainer != nil){
+            tabBarContainer?.didClickOnAccountBar(accountOptionsWereOpen)
+        }
+    }
+    
+    // MARK: SwitchRoomViewDelegate
+    func didSelectRoom(rid: String, roomName: String) {
+        // not much to do here, simply let the view controller know about the event, so it can swap the rooms.
+        viewController!.didSelectRoom(rid, roomName: roomName)
+        
+    }
+    
 }
