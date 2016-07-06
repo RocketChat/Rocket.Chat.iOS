a067710279c21783286069f823f425174506cbef
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2016-07-06 18:46:42 -0300

    Start working with queues to Socket request/response

---
+++ Rocket.Chat/Extensions/StringExtensions.swift
@@ -0,0 +1,25 @@
+//
+//  StringExtensions.swift
+//  Rocket.Chat
+//
+//  Created by Rafael K. Streit on 7/6/16.
+//  Copyright © 2016 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+    static func random(length: Int = 20) -> String {
+        let base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        var randomString: String = ""
+        
+        for _ in 0..<length {
+            let randomValue = arc4random_uniform(UInt32(base.characters.count))
+            randomString += "\(base[base.startIndex.advancedBy(Int(randomValue))])"
+        }
+        
+        return randomString
+    }
+
+}
