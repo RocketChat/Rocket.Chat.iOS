ebc917ffb55a0fd4eb49502ce461e0d4821783dd
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2016-07-06 17:04:51 -0300

    Initial server setup

---
+++ Rocket.Chat/Controllers/Auth/AuthViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AuthViewController.swift
+//  Rocket.Chat
+//
+//  Created by Rafael K. Streit on 7/6/16.
+//  Copyright © 2016 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AuthViewController: BaseViewController {
+    
+    var serverURL: String?
+    
+}
---
+++ Rocket.Chat/Controllers/Base/BaseViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BaseViewController.swift
+//  Rocket.Chat
+//
+//  Created by Rafael K. Streit on 7/6/16.
+//  Copyright © 2016 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+}
