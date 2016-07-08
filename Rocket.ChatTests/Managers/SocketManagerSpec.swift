8eecab160d584c5e85adeb195ae9ce6e69109b08
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2016-07-08 16:41:08 -0300

    Create tests for SocketResult object

---
+++ Rocket.ChatTests/Managers/SocketManagerSpec.swift
@@ -0,0 +1,40 @@
+//
+//  SocketManagerSpec.swift
+//  Rocket.Chat
+//
+//  Created by Rafael K. Streit on 7/8/16.
+//  Copyright © 2016 Rocket.Chat. All rights reserved.
+//
+
+import XCTest
+import SwiftyJSON
+
+class SocketManagerSpec: XCTestCase {
+    
+    // MARK: SocketResponse
+    
+    func testSocketResponseErrorMethodMsgError() {
+        let result = JSON(["msg": "error", "foo": "bar"])
+        let response = SocketResponse(result: result, socket: nil)
+        XCTAssert(response.isError() == true, "isError() should return true when JSON object contains {'msg': 'error'}")
+    }
+    
+    func testSocketResponseErrorMethodMsgFailed() {
+        let result = JSON(["msg": "failed", "foo": "bar"])
+        let response = SocketResponse(result: result, socket: nil)
+        XCTAssert(response.isError() == true, "isError() should return true when JSON object contains {'msg': 'failed'}")
+    }
+    
+    func testSocketResponseErrorObjectError() {
+        let result = JSON(["msg": "result", "error": []])
+        let response = SocketResponse(result: result, socket: nil)
+        XCTAssert(response.isError() == true, "isError() should return true when JSON object contains {'error': [...]}")
+    }
+    
+    func testSocketResponseErrorFalse() {
+        let result = JSON(["msg": "result", "foo": "bar"])
+        let response = SocketResponse(result: result, socket: nil)
+        XCTAssert(response.isError() == false, "isError() should return true when JSON don't contains error")
+    }
+    
+}
