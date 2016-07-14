e0e1269a9a902cd3dcd4c8655f770e4d840c50d3
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2016-07-14 19:30:01 -0300

    Map messages from JSON

--- Rocket.Chat/Managers/Model/MessageManager.swift
+++ Rocket.Chat/Managers/Model/MessageManager.swift
@@ -7,9 +7,53 @@
 //
 
 import Foundation
+import RealmSwift
 
 class MessageManager {
     
+    static let historySize = 50
     
+    static func fetchHistory(subscription: Subscription, completion: MessageCompletion) {
+        let request = [
+            "msg": "method",
+            "method": "loadHistory",
+            "params": ["xSgZjSaWrcXoDR4QZ", NSNull(), historySize, [
+                "$date": NSDate().timeIntervalSince1970 * 1000
+            ]]
+        ]
+        
+        SocketManager.sendMessage(request) { (response) in
+            guard !response.isError() else {
+                return print(response.result)
+            }
+            
+            let messages = List<Message>()
+            if let result = response.result["result"]["messages"].array {
+                for obj in result {
+                    let message = Message()
+                    message.identifier = obj["_id"].string!
+                    message.rid = obj["rid"].string!
+                    message.text = obj["msg"].string!
+                    
+                    if let createdAt = obj["ts"]["$date"].double {
+                        message.createdAt = NSDate.dateFromInterval(createdAt)
+                    }
+                    
+                    if let updatedAt = obj["_updatedAt"]["$date"].double {
+                        message.updatedAt = NSDate.dateFromInterval(updatedAt)
+                    }
+                    
+                    messages.append(message)
+                }
+            }
+            
+            let realm = try! Realm()
+            try! realm.write {
+                realm.add(messages, update: true)
+            }
+            
+            completion(response)
+        }
+    }
 
 }
