ff3383c30487a738465bde13b33f6a11ced4aec1
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2016-07-14 19:18:25 -0300

    Create initial Message/Mentio model & manager

---
+++ Rocket.Chat/Managers/Model/MessageManager.swift
@@ -0,0 +1,15 @@
+//
+//  MessageManager.swift
+//  Rocket.Chat
+//
+//  Created by Rafael K. Streit on 7/14/16.
+//  Copyright © 2016 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
+
+class MessageManager {
+    
+    
+
+}
