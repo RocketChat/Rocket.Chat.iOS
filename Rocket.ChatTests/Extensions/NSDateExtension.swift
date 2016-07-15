89b1770f0dd721238b842e70a7182fba7c4c4c5c
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2016-07-14 22:05:41 -0300

    Write a few tests to NSDateExtension

---
+++ Rocket.ChatTests/Extensions/NSDateExtension.swift
@@ -0,0 +1,21 @@
+//
+//  NSDateExtension.swift
+//  Rocket.Chat
+//
+//  Created by Rafael K. Streit on 7/14/16.
+//  Copyright © 2016 Rocket.Chat. All rights reserved.
+//
+
+import XCTest
+
+@testable import Rocket_Chat
+
+class NSDateExtension: XCTestCase {
+
+    func testDateFromInterval() {
+        let interval = 1468544344553.0
+        let date = NSDate.dateFromInterval(interval)
+        XCTAssert(date?.timeIntervalSince1970 == interval / 1000, "Date interval is divided by 1000")
+    }
+
+}
