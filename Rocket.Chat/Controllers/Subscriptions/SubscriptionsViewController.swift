db924c5dd36bfd4742e47572269d4658755a0ce5
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2016-07-21 16:09:00 -0300

    Show the subscriptions list in the left menu

--- Rocket.Chat/Controllers/Subscriptions/SubscriptionsViewController.swift
+++ Rocket.Chat/Controllers/Subscriptions/SubscriptionsViewController.swift
@@ -14,3 +14,15 @@ class SubscriptionsViewController: BaseViewController {
     @IBOutlet weak var tableView: UITableView!
     
 }
+
+extension SubscriptionsViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
