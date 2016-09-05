d6741af2f01fd1908885d9facbd2c6264f48b2cc
Rafael Kellermann Streit <rafaelks@me.com>
2016-09-05 15:30:46 -0300

    Add initial integration with Fabric/Crashlytics

--- Rocket.Chat/AppDelegate.swift
+++ Rocket.Chat/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        Fabric.with([Crashlytics.self])
         return true
     }
 
