a0348b37b24b792af39658966448d7967e864e83
CodeBanBan <codebanban@gmail.com>
2016-09-16 21:41:50 +0700

    group subscription by SubscriptionType

--- Rocket.Chat/Controllers/Subscriptions/SubscriptionsViewController.swift
+++ Rocket.Chat/Controllers/Subscriptions/SubscriptionsViewController.swift
@@ -10,69 +10,127 @@ import RealmSwift
 import SideMenu
 
 class SubscriptionsViewController: BaseViewController {
-    
-    @IBOutlet weak var tableView: UITableView!
-    
-    var assigned = false
-    var subscriptions: Results<Subscription>?
-    var subscriptionsToken: NotificationToken?
-    var usersToken: NotificationToken?
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        subscribeModelChanges()
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        tableView.reloadData()
-    }
-    
+	
+	@IBOutlet weak var tableView: UITableView!
+	
+	var assigned = false
+	var subscriptions: Results<Subscription>?
+	var subscriptionsToken: NotificationToken?
+	var usersToken: NotificationToken?
+	
+	var groupInfomation: [[String: String]]?
+	var groupSubscriptions: [List<Subscription>]?
+	
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		subscribeModelChanges()
+	}
+	
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+		tableView.reloadData()
+	}
+	
 }
 
 extension SubscriptionsViewController {
-    
-    func handleModelUpdates<T>(_: RealmCollectionChange<RealmSwift.Results<T>>) {
-        tableView?.reloadData()
-    }
-    
-    func subscribeModelChanges() {
-        guard !assigned else { return }
-        guard let auth = AuthManager.isAuthenticated() else { return }
-        
-        assigned = true
-        
-        subscriptions = auth.subscriptions.sorted("lastSeen", ascending: false)
-        subscriptionsToken = subscriptions?.addNotificationBlock(handleModelUpdates)
-        usersToken = try! Realm().objects(User.self).addNotificationBlock(handleModelUpdates)
-    }
-    
+	
+	func handleModelUpdates<T>(_: RealmCollectionChange<RealmSwift.Results<T>>) {
+		tableView?.reloadData()
+	}
+	
+	func subscribeModelChanges() {
+		guard !assigned else { return }
+		guard let auth = AuthManager.isAuthenticated() else { return }
+		
+		assigned = true
+		
+		subscriptions = auth.subscriptions.sorted("lastSeen", ascending: false)
+		subscriptionsToken = subscriptions?.addNotificationBlock(handleModelUpdates)
+		usersToken = try! Realm().objects(User.self).addNotificationBlock(handleModelUpdates)
+		
+		groupSubscription()
+	}
+	
+	func groupSubscription() {
+		let favoriteGroup: List<Subscription> = List<Subscription>()
+		let channelGroup: List<Subscription> = List<Subscription>()
+		let directMessageGroup: List<Subscription> = List<Subscription>()
+		
+		for subscription in subscriptions! {
+			if (!subscription.open) {
+				continue
+			}
+			
+			if (subscription.favorite) {
+				favoriteGroup.append(subscription)
+				continue
+			}
+			
+			switch subscription.type {
+			case .Channel,
+			     .Group:
+				channelGroup.append(subscription)
+				break
+			case .DirectMessage:
+				directMessageGroup.append(subscription)
+				break
+			}
+		}
+		
+		groupInfomation = [[String: String]]()
+		groupSubscriptions = [List<Subscription>]()
+		
+		if (favoriteGroup.count > 0) {
+			groupInfomation?.append(["name": "Favorites"])
+			groupSubscriptions?.append(favoriteGroup)
+		}
+		
+		if (channelGroup.count > 0) {
+			groupInfomation?.append(["name": "Channels"])
+			groupSubscriptions?.append(channelGroup)
+		}
+		
+		if (directMessageGroup.count > 0) {
+			groupInfomation?.append(["name": "Direct Messages"])
+			groupSubscriptions?.append(directMessageGroup)
+		}
+	}
 }
 
 extension SubscriptionsViewController: UITableViewDataSource {
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return subscriptions?.count ?? 0
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let subscription = subscriptions![indexPath.row]
-
-        let cell = tableView.dequeueReusableCellWithIdentifier(SubscriptionCell.identifier) as! SubscriptionCell
-        cell.subscription = subscription
-
-        return cell
-    }
-    
+	
+	func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		return groupInfomation?.count ?? 0
+	}
+	
+	func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		return groupInfomation?[section]["name"] ?? ""
+	}
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return groupSubscriptions?[section].count ?? 0
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let subscription = groupSubscriptions?[indexPath.section][indexPath.row]
+		
+		let cell = tableView.dequeueReusableCellWithIdentifier(SubscriptionCell.identifier) as! SubscriptionCell
+		cell.subscription = subscription
+		
+		return cell
+	}
+	
 }
 
 
 extension SubscriptionsViewController: UITableViewDelegate {
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let subscription = subscriptions![indexPath.row]
-        ChatViewController.sharedInstance()?.subscription = subscription
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-    
+	
+	func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		let subscription = groupSubscriptions?[indexPath.section][indexPath.row]
+		
+		ChatViewController.sharedInstance()?.subscription = subscription
+		dismissViewControllerAnimated(true, completion: nil)
+	}
+	
 }
