69ed2049f37f0c9500226401f26aea9ab0966e8e
Rafael Kellermann Streit <rafaelks@me.com>
2016-10-01 09:45:01 -0300

    Define all attributes of Attachment model

--- Rocket.Chat/Models/Attachment.swift
+++ Rocket.Chat/Models/Attachment.swift
@@ -15,6 +15,24 @@ enum AttachmentType {
 }
 
 class Attachment: BaseModel {
-    dynamic var email = ""
-    dynamic var verified = false
+    var type: AttachmentType {
+        get { return .image }
+    }
+    
+    dynamic var title = ""
+    dynamic var title_link = ""
+    dynamic var title_link_download = true
+    
+    dynamic var image_url = ""
+    dynamic var image_type = ""
+    dynamic var image_size = ""
+    dynamic var image_dimensions = ""
+    
+    dynamic var audio_url = ""
+    dynamic var audio_type = ""
+    dynamic var audio_size = ""
+    
+    dynamic var video_url = ""
+    dynamic var video_type = ""
+    dynamic var video_size = ""
 }
