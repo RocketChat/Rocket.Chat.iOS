c234f8b574c04bd7dfa7f420a5cbf3baf12ff3e3
Rafael Machado <raka.cdc@gmail.com>
2016-10-12 19:39:03 -0300

    Add NibLoadable (will create view from xib files) Add Autolayout constraints migration (to work in conjunction with NibLoadable and make xib reusable) Create BaseView to encapsulate the reusable logic

---
+++ Rocket.Chat/Views/BaseView.swift
@@ -0,0 +1,33 @@
+//
+//  BaseView.swift
+//  Rocket.Chat
+//
+//  Created by Rafael Machado on 12/10/16.
+//  Copyright © 2016 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+
+
+class BaseView: UIView, NibLoadableView {
+    
+    func isReplaceable() -> Bool {
+        return false
+    }
+    
+    override func awakeAfter(using aDecoder: NSCoder) -> Any? {
+    
+        if !isReplaceable() || !subviews.isEmpty {
+            return self
+        }
+        
+        let repleacement = type(of: self).instanceFromNib()
+        repleacement.tag = tag
+        repleacement.frame = frame
+        repleacement.translatesAutoresizingMaskIntoConstraints = translatesAutoresizingMaskIntoConstraints
+        
+        repleacement.addConstraints(migrateConstraintsToView(view: repleacement))
+        
+        return repleacement
+    }
+}
