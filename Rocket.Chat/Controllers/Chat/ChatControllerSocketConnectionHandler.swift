c8563b77e99d955776d4b765ec4b01a59bcfc7fa
Rafael Kellermann Streit <rafaelks@me.com>
2016-12-17 16:47:38 -0200

    Separate few of the logic in other files

---
+++ Rocket.Chat/Controllers/Chat/ChatControllerSocketConnectionHandler.swift
@@ -0,0 +1,29 @@
+//
+//  ChatControllerSocketConnectionHandler.swift
+//  Rocket.Chat
+//
+//  Created by Rafael Kellermann Streit on 17/12/16.
+//  Copyright © 2016 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+
+
+extension ChatViewController: SocketConnectionHandler {
+    
+    func socketDidConnect(socket: SocketManager) {
+        chatHeaderViewOffline?.removeFromSuperview()
+        updateSubscriptionMessages()
+        rightButton.isEnabled = true
+    }
+    
+    func socketDidDisconnect(socket: SocketManager) {
+        chatHeaderViewOffline?.removeFromSuperview()
+        
+        let headerView = ChatHeaderViewOffline.instanceFromNib() as! ChatHeaderViewOffline
+        headerView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: headerView.frame.height)
+        view.addSubview(headerView)
+        chatHeaderViewOffline = headerView
+    }
+    
+}
