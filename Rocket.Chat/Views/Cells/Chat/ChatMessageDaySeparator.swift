33dda0254b6511fd1f975a763964ae5775a6001f
Rafael Kellermann Streit <rafaelks@me.com>
2016-12-19 11:48:36 -0200

    Add the daySeparator cell to the collectionView

---
+++ Rocket.Chat/Views/Cells/Chat/ChatMessageDaySeparator.swift
@@ -0,0 +1,18 @@
+//
+//  ChatMessageDaySeparator.swift
+//  Rocket.Chat
+//
+//  Created by Rafael Kellermann Streit on 19/12/16.
+//  Copyright © 2016 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+
+class ChatMessageDaySeparator: UICollectionViewCell {
+    
+    static let minimumHeight = CGFloat(30)
+    static let identifier = "ChatMessageDaySeparator"
+    
+    @IBOutlet weak var labelTitle: UILabel!
+
+}
