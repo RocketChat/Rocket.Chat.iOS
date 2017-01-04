f5339fbc756b24ab516c808517b8bef88b02d388
rakaramos <raka.cdc@gmail.com>
2017-01-04 13:22:37 -0200

    Clean up swiftlint script

--- Scripts/swiftlint.sh
+++ Scripts/swiftlint.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+function lint() {
+	echo "Running SwiftLint..."
+	swiftlint
+}
+
 if which swiftlint >/dev/null; then
-  swiftlint
+  lint
 else
     # lets try to install swift lint using homebrew
     if which brew >/dev/null; then
-        if result=$(brew install swiftlint); then
-            swiftlint
+        if $(brew update && brew install swiftlin); then
+            lint
             exit 0
         else
             echo "Failed to install swiftlint using homebrew"
-            echo "$result"
             exit 1
         fi
     else
-        echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+        echo "error: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
         exit 1
     fi
     exit 1
