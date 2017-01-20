4eb2923c1a4f15d2e78765ef89659e480147e71c
Rafael Kellermann Streit <rafaelks@me.com>
2017-01-20 10:28:34 -0200

    Add settings that RC supports for S3

--- Rocket.Chat/Models/AuthSettings.swift
+++ Rocket.Chat/Models/AuthSettings.swift
@@ -12,4 +12,16 @@ import SwiftyJSON
 
 final class AuthSettings: BaseModel {
     dynamic var siteURL: String?
+
+    // File upload
+    dynamic var uploadStorageType: String?
+
+    // Amazon S3 settings
+    dynamic var AWSS3Bucket: String?
+    dynamic var AWSS3BucketURL: String?
+    dynamic var AWSS3ACL: String?
+    dynamic var AWSS3AccessKey: String?
+    dynamic var AWSS3SecretKey: String?
+    dynamic var AWSS3CDN: String?
+    dynamic var AWSS3Region: String?
 }
