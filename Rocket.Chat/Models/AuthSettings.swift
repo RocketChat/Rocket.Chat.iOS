2fb43cbfe5983d644f208a73349b4f75285a90d0
Rafael Kellermann Streit <rafaelks@me.com>
2017-02-01 20:39:28 -0200

    Send file to the chat

--- Rocket.Chat/Models/AuthSettings.swift
+++ Rocket.Chat/Models/AuthSettings.swift
@@ -15,13 +15,4 @@ final class AuthSettings: BaseModel {
 
     // File upload
     dynamic var uploadStorageType: String?
-
-    // Amazon S3 settings
-    dynamic var AWSS3Bucket: String?
-    dynamic var AWSS3BucketURL: String?
-    dynamic var AWSS3ACL: String?
-    dynamic var AWSS3AccessKey: String?
-    dynamic var AWSS3SecretKey: String?
-    dynamic var AWSS3CDN: String?
-    dynamic var AWSS3Region: String?
 }
