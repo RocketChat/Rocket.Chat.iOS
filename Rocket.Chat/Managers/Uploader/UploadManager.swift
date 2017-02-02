2300621944f2a6a0bce8f42cce96fb0d590690f4
Rafael Kellermann Streit <rafaelks@me.com>
2017-02-02 09:48:52 -0200

    Optimize a few things

--- Rocket.Chat/Managers/Uploader/UploadManager.swift
+++ Rocket.Chat/Managers/Uploader/UploadManager.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 public typealias UploadProgressBlock = (Int) -> Void
 public typealias UploadCompletionBlock = (Bool) -> Void
@@ -34,6 +35,33 @@ class UploadManager {
         }
     }
 
+    fileprivate func requestUpload(_ url: URL, file: FileUpload, formData: JSON) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+        let boundary = String.random()
+        let contentType = "multipart/form-data; boundary=\(boundary)"
+        request.addValue(contentType, forHTTPHeaderField: "Content-Type")
+
+        var data = Data()
+        data.append("\r\n--\(boundary)\r\n".data(using: .utf8) ?? Data())
+
+        for postData in formData.array ?? [] {
+            guard let key = postData["name"].string else { continue }
+            guard let value = postData["value"].string else { continue }
+            data.append("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n".data(using: .utf8) ?? Data())
+            data.append(value.data(using: .utf8) ?? Data())
+            data.append("\r\n--\(boundary)\r\n".data(using: .utf8) ?? Data())
+        }
+
+        data.append("Content-Disposition: form-data; name=\"file\"\r\n".data(using: .utf8) ?? Data())
+        data.append("Content-Type: application/octet-stream\r\n\r\n".data(using: .utf8) ?? Data())
+        data.append(file.data)
+        data.append("\r\n--\(boundary)--\r\n".data(using: .utf8) ?? Data())
+        request.httpBody = data
+        return request
+    }
+
     func upload(file: FileUpload, subscription: Subscription, progress: UploadProgressBlock, completion: @escaping UploadCompletionBlock) {
         let request = [
             "msg": "method",
@@ -49,7 +77,7 @@ class UploadManager {
             ]
         ] as [String : Any]
 
-        SocketManager.send(request) { (response) in
+        SocketManager.send(request) { [unowned self] (response) in
             guard !response.isError() else {
                 return
             }
@@ -58,39 +86,14 @@ class UploadManager {
             guard let uploadURL = URL(string: result["result"]["upload"].string ?? "") else { return }
             guard let downloadURL = result["result"]["download"].string else { return }
 
-            var request = URLRequest(url: uploadURL)
-            request.httpMethod = "POST"
-
-            let boundary = String.random()
-            let contentType = "multipart/form-data; boundary=\(boundary)"
-            request.addValue(contentType, forHTTPHeaderField: "Content-Type")
-
-            var data = Data()
-            data.append("\r\n--\(boundary)\r\n".data(using: .utf8) ?? Data())
-
-            for postData in result["result"]["postData"].array ?? [] {
-                guard let key = postData["name"].string else { continue }
-                guard let value = postData["value"].string else { continue }
-                data.append("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n".data(using: .utf8) ?? Data())
-                data.append(value.data(using: .utf8) ?? Data())
-                data.append("\r\n--\(boundary)\r\n".data(using: .utf8) ?? Data())
-            }
-
-            data.append("Content-Disposition: form-data; name=\"file\"\r\n".data(using: .utf8) ?? Data())
-            data.append("Content-Type: application/octet-stream\r\n\r\n".data(using: .utf8) ?? Data())
-            data.append(file.data)
-            data.append("\r\n--\(boundary)--\r\n".data(using: .utf8) ?? Data())
-            request.httpBody = data
+            let request = self.requestUpload(uploadURL, file: file, formData: result["result"]["postData"])
 
             let config = URLSessionConfiguration.default
             let session = URLSession(configuration: config)
-            let task = session.dataTask(with: request, completionHandler: { (_, response, error) in
+            let task = session.dataTask(with: request, completionHandler: { (_, _, error) in
                 if let _ = error {
-                    print(error)
                     completion(false)
                 } else {
-                    print(response)
-
                     DispatchQueue.main.async {
                         let fileIdentifier = downloadURL.components(separatedBy: "/").last
 
@@ -113,3 +116,4 @@ class UploadManager {
     }
 
 }
+
