182062e7a11c8008d6143fe57c4700bfbaafe859
Rafael Kellermann Streit <rafaelks@me.com>
2017-02-13 10:46:03 -0200

    Localize all strings and fix icons alignment

--- Rocket.Chat/Views/Subscriptions/SubscriptionUserStatusView.swift
+++ Rocket.Chat/Views/Subscriptions/SubscriptionUserStatusView.swift
@@ -10,9 +10,51 @@ import UIKit
 
 class SubscriptionUserStatusView: BaseView {
 
+    @IBOutlet weak var viewOnline: UIView!
+    @IBOutlet weak var labelOnline: UILabel! {
+        didSet {
+            labelOnline.text = localizedString("user_menu.online")
+        }
+    }
+
+    @IBOutlet weak var viewAway: UIView!
+    @IBOutlet weak var labelAway: UILabel! {
+        didSet {
+            labelAway.text = localizedString("user_menu.away")
+        }
+    }
+
+    @IBOutlet weak var viewBusy: UIView!
+    @IBOutlet weak var labelBusy: UILabel! {
+        didSet {
+            labelBusy.text = localizedString("user_menu.busy")
+        }
+    }
+
+    @IBOutlet weak var viewInvisible: UIView!
+    @IBOutlet weak var labelInvisible: UILabel! {
+        didSet {
+            labelInvisible.text = localizedString("user_menu.invisible")
+        }
+    }
+
+    @IBOutlet weak var viewSettings: UIView!
+    @IBOutlet weak var labelSettings: UILabel! {
+        didSet {
+            labelSettings.text = localizedString("user_menu.settings")
+        }
+    }
+
     @IBOutlet weak var imageViewSettings: UIImageView! {
         didSet {
             imageViewSettings.image = imageViewSettings.image?.imageWithTint(.RCLightBlue())
+        }
+    }
+
+    @IBOutlet weak var viewLogout: UIView!
+    @IBOutlet weak var labelLogout: UILabel! {
+        didSet {
+            labelLogout.text = localizedString("user_menu.logout")
         }
     }
 
