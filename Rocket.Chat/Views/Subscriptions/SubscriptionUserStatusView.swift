3c467e7d19c083779f308fd801276278326f980f
Rafael Kellermann Streit <rafaelks@me.com>
2017-02-13 10:59:19 -0200

    Add a button to each option

--- Rocket.Chat/Views/Subscriptions/SubscriptionUserStatusView.swift
+++ Rocket.Chat/Views/Subscriptions/SubscriptionUserStatusView.swift
@@ -10,35 +10,35 @@ import UIKit
 
 class SubscriptionUserStatusView: BaseView {
 
-    @IBOutlet weak var viewOnline: UIView!
+    @IBOutlet weak var buttonOnline: UIButton!
     @IBOutlet weak var labelOnline: UILabel! {
         didSet {
             labelOnline.text = localizedString("user_menu.online")
         }
     }
 
-    @IBOutlet weak var viewAway: UIView!
+    @IBOutlet weak var buttonAway: UIView!
     @IBOutlet weak var labelAway: UILabel! {
         didSet {
             labelAway.text = localizedString("user_menu.away")
         }
     }
 
-    @IBOutlet weak var viewBusy: UIView!
+    @IBOutlet weak var buttonBusy: UIView!
     @IBOutlet weak var labelBusy: UILabel! {
         didSet {
             labelBusy.text = localizedString("user_menu.busy")
         }
     }
 
-    @IBOutlet weak var viewInvisible: UIView!
+    @IBOutlet weak var buttonInvisible: UIView!
     @IBOutlet weak var labelInvisible: UILabel! {
         didSet {
             labelInvisible.text = localizedString("user_menu.invisible")
         }
     }
 
-    @IBOutlet weak var viewSettings: UIView!
+    @IBOutlet weak var buttonSettings: UIView!
     @IBOutlet weak var labelSettings: UILabel! {
         didSet {
             labelSettings.text = localizedString("user_menu.settings")
@@ -51,7 +51,7 @@ class SubscriptionUserStatusView: BaseView {
         }
     }
 
-    @IBOutlet weak var viewLogout: UIView!
+    @IBOutlet weak var buttonLogout: UIView!
     @IBOutlet weak var labelLogout: UILabel! {
         didSet {
             labelLogout.text = localizedString("user_menu.logout")
@@ -62,6 +62,33 @@ class SubscriptionUserStatusView: BaseView {
         didSet {
             imageViewLogout.image = imageViewLogout.image?.imageWithTint(.RCLightBlue())
         }
+    }
+
+
+    // MARK: IBAction
+
+    @IBAction func buttonOnlineDidPressed(_ sender: Any) {
+
+    }
+
+    @IBAction func buttonAwayDidPressed(_ sender: Any) {
+
+    }
+
+    @IBAction func buttonBusyDidPressed(_ sender: Any) {
+
+    }
+
+    @IBAction func buttonInvisibleDidPressed(_ sender: Any) {
+
+    }
+
+    @IBAction func buttonSettingsDidPressed(_ sender: Any) {
+
+    }
+
+    @IBAction func buttonLogoutDidPressed(_ sender: Any) {
+        
     }
 
 }
