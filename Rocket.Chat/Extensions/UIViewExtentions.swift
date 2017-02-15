f232544262cdf5c624529c0b93e2f1a61bf201df
Rafael Kellermann Streit <rafaelks@me.com>
2017-02-15 09:30:30 -0200

    Simplify a lot the instantiacion from a nib file, without casting it

--- Rocket.Chat/Extensions/UIViewExtentions.swift
+++ Rocket.Chat/Extensions/UIViewExtentions.swift
@@ -9,87 +9,20 @@
 import Foundation
 import UIKit
 
-/**
- * Easy way to migrate autolayout constraints
- * This is meant to be used in conjunction with NibLoadableView
- */
-public extension UIView {
 
-    func migrateToSuperview(superview: UIView) {
-        let previousSuperview = self.superview
-        let constraints = previousSuperview?.removeConstraintsInvolvingView(view: self) ?? [NSLayoutConstraint]()
-        let migratedConstratints = previousSuperview?.migrateConstraints(constraints: constraints, toView: superview) ?? [NSLayoutConstraint]()
+extension UIView {
 
-        superview.addSubview(self)
-        superview.addConstraints(migratedConstratints)
+    static var nib: UINib {
+        return UINib(nibName: "\(self)", bundle: nil)
     }
 
-    func migrateConstraintsToView(view: UIView) -> [NSLayoutConstraint] {
-
-        return migrateConstraints(constraints: constraints, toView: view)
-    }
-
-    func removeConstraintsInvolvingView(view: UIView) -> [NSLayoutConstraint] {
-        let constraints = constraintsInvolvingView(view: view)
-        removeConstraints(constraints)
-        return constraints
-    }
-
-    func migrateConstraints(constraints: [NSLayoutConstraint], toView view: UIView) -> [NSLayoutConstraint] {
-        if constraints.count <= 0 {
-            return []
+    static func instantiateFromNib() -> Self? {
+        func instanceFromNib<T: UIView>() ->T? {
+            return nib.instantiate() as? T
         }
 
-        return constraints.map { constraint in
-            return migrateConstraint(constraint: constraint, toView: view)
-        }
-    }
-
-    func constraintsInvolvingView(view: UIView) -> [NSLayoutConstraint] {
-        return constraints.filter { constraint in
-            let firstItem  = (constraint.firstItem as? UIView) ?? UIView()
-            let secondItem = (constraint.secondItem as? UIView) ?? UIView()
-            return firstItem == view || secondItem == view
-        }
-    }
-
-    // Helpers
-
-    private func migrateConstraint(constraint: NSLayoutConstraint, toView view: UIView) -> NSLayoutConstraint {
-        var firstAttribute  = constraint.firstAttribute
-        var secondAttribute = constraint.secondAttribute
-
-        guard let firstItem  = migrateItem(item: constraint.firstItem, attribute:&firstAttribute, toView: view) else {
-            return constraint
-        }
-
-        let secondItem = migrateItem(item: constraint.secondItem, attribute:&secondAttribute, toView: view)
-
-        return NSLayoutConstraint.init(item:firstItem, attribute: firstAttribute, relatedBy: constraint.relation, toItem: secondItem, attribute: secondAttribute, multiplier: constraint.multiplier, constant: constraint.constant)
-    }
-
-    private func migrateItem(item: AnyObject?, attribute anAttribute:inout NSLayoutAttribute, toView view: UIView) -> AnyObject? {
-        var result = item
-
-        if (item as? NSObject) == self {
-            result = view
-        } else if let _ = item as? UILayoutSupport {
-            result = view
-            anAttribute = invertAttribute(attribute: anAttribute)
-        }
-
-        return result
-    }
-
-    private func invertAttribute(attribute: NSLayoutAttribute) -> NSLayoutAttribute {
-        switch attribute {
-        case .top:
-            return .bottom
-        case .bottom:
-            return .top
-        default:
-            return attribute
-        }
+        return instanceFromNib()
     }
 
 }
+
