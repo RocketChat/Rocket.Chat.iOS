8de527bd1afb88b7574ab75d0980dec85de3d130
gradler <gradlerkim@gmail.com>
2017-02-20 10:45:05 +0900

    feature added favorite & unfavorite option in the subscription

--- Rocket.Chat/Managers/Model/SubscriptionManager.swift
+++ Rocket.Chat/Managers/Model/SubscriptionManager.swift
@@ -212,4 +212,17 @@ struct SubscriptionManager {
             completion(response)
         }
     }
+    
+    static func toggleFavorite(_ subscription: Subscription, completion: @escaping MessageCompletion) {
+        let request = [
+            "msg": "method",
+            "method": "toggleFavorite",
+            "params": [subscription.rid, !subscription.favorite]
+            ] as [String : Any]
+        
+        SocketManager.send(request) { (response) in
+            guard !response.isError() else { return Log.debug(response.result.string) }
+            completion(response)
+        }
+    }
 }
