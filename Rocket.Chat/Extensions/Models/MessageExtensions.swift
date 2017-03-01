79c6a2984e37906f3b228498e849eb96a6ad2aa8
Rafael Kellermann Streit <rafaelks@me.com>
2017-03-01 17:34:11 -0300

    Make a better code separation into the message styling

--- Rocket.Chat/Extensions/Models/MessageExtensions.swift
+++ Rocket.Chat/Extensions/Models/MessageExtensions.swift
@@ -10,108 +10,103 @@ import UIKit
 
 extension Message {
 
-    func textNormalized() -> NSAttributedString {
+    func isSystemMessage() -> Bool {
+        return !(
+            type == .text ||
+            type == .audio ||
+            type == .image ||
+            type == .video ||
+            type == .textAttachment ||
+            type == .url
+        )
+    }
+
+    func textNormalized() -> String {
+        let text = Emojione.transform(string: self.text)
+
         switch type {
             case .roomNameChanged:
-                return grayItalicText(String(
+                return String(
                     format: localizedString("chat.message.type.room_name_changed"),
                     text,
                     self.user?.username ?? ""
-                ))
+                )
 
             case .userAdded:
-                return grayItalicText(String(
+                return String(
                     format: localizedString("chat.message.type.user_added_by"),
                     text,
                     self.user?.username ?? ""
-                ))
+                )
 
             case .userRemoved:
-                return grayItalicText(String(
+                return String(
                     format: localizedString("chat.message.type.user_removed_by"),
                     text,
                     self.user?.username ?? ""
-                ))
+                )
 
             case .userJoined:
-                return grayItalicText(localizedString("chat.message.type.user_joined"))
+                return localizedString("chat.message.type.user_joined")
 
             case .userLeft:
-                return grayItalicText(localizedString("chat.message.type.user_left"))
+                return localizedString("chat.message.type.user_left")
 
             case .userMuted:
-                return grayItalicText(String(
+                return String(
                     format: localizedString("chat.message.type.user_muted"),
                     text,
                     self.user?.username ?? ""
-                ))
+                )
 
             case .userUnmuted:
-                return grayItalicText(String(
+                return String(
                     format: localizedString("chat.message.type.user_unmuted"),
                     text,
                     self.user?.username ?? ""
-                ))
+                )
 
             case .welcome:
-                return grayItalicText(String(
+                return String(
                     format: localizedString("chat.message.type.welcome"),
                     text
-                ))
+                )
 
             case .messageRemoved:
-                return grayItalicText(localizedString("chat.message.type.message_removed"))
+                return localizedString("chat.message.type.message_removed")
 
             case .subscriptionRoleAdded:
-                return grayItalicText(String(
+                return String(
                     format: localizedString("chat.message.type.subscription_role_added"),
                     text,
                     role,
                     self.user?.username ?? ""
-                ))
+                )
 
             case .subscriptionRoleRemoved:
-                return grayItalicText(String(
+                return String(
                     format: localizedString("chat.message.type.subscription_role_removed"),
                     text,
                     role,
                     self.user?.username ?? ""
-                ))
+                )
 
             case .roomArchived:
-                return grayItalicText(String(
+                return String(
                     format: localizedString("chat.message.type.room_archived"),
                     text
-                ))
+                )
 
             case .roomUnarchived:
-                return grayItalicText(String(
+                return String(
                     format: localizedString("chat.message.type.room_unarchived"),
                     text
-                ))
+                )
 
             default: break
         }
 
-        return defaultMessageText(Emojione.transform(string: text))
-    }
-
-    fileprivate func defaultMessageText(_ string: String) -> NSAttributedString {
-        let mutableString = NSMutableAttributedString(string: string)
-        mutableString.addAttributes([
-            NSFontAttributeName: UIFont.systemFont(ofSize: 14),
-            NSForegroundColorAttributeName: UIColor.darkGray
-        ], range: NSRange(location: 0, length: string.characters.count))
-        return mutableString
-    }
-
-    fileprivate func grayItalicText(_ string: String) -> NSAttributedString {
-        let mutableString = NSMutableAttributedString(string: string)
-        mutableString.addAttributes([
-            NSFontAttributeName: UIFont.italicSystemFont(ofSize: 14),
-            NSForegroundColorAttributeName: UIColor.lightGray
-        ], range: NSRange(location: 0, length: string.characters.count))
-        return mutableString
+        return text
     }
 
 }
---
+++ Rocket.Chat/Extensions/NSAttributedStringExtensions.swift
@@ -0,0 +1,29 @@
+//
+//  NSAttributedStringExtensions.swift
+//  Rocket.Chat
+//
+//  Created by Rafael Kellermann Streit on 01/03/17.
+//  Copyright © 2017 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+
+extension NSMutableAttributedString {
+
+    func setFont(_ font: UIFont, range: NSRange? = nil) {
+        if let attributeRange = range != nil ? range : NSRange(location: 0, length: self.length) {
+            self.addAttributes([
+                NSFontAttributeName: font,
+            ], range: attributeRange)
+        }
+    }
+
+    func setFontColor(_ color: UIColor, range: NSRange? = nil) {
+        if let attributeRange = range != nil ? range : NSRange(location: 0, length: self.length) {
+            self.addAttributes([
+                NSForegroundColorAttributeName: color,
+            ], range: attributeRange)
+        }
+    }
+
+}
