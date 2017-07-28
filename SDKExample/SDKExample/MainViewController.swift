289cfd5c053e4da56b27b7e7d34f9a7aff240a7d
Rafael Kellermann Streit <rafaelks@me.com>
2017-07-28 11:02:40 -0300

    Remove unused code to clean it

--- SDKExample/SDKExample/MainViewController.swift
+++ SDKExample/SDKExample/MainViewController.swift
@@ -15,26 +15,6 @@ class MainViewController: UIViewController {
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var secureSwitch: UISwitch!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-
     @IBAction func didTouchSupportButton(_ sender: Any) {
         presentSupportViewController()
     }
@@ -42,11 +22,17 @@ class MainViewController: UIViewController {
     func presentSupportViewController() {
         guard let serverAddr = serverAddressField.text else { return }
         guard let serverUrl = URL(string: serverAddr) else {
-            let alert = UIAlertController(title: "Validation Error", message: "Server Address is not a valid URL", preferredStyle: .alert)
+            let alert = UIAlertController(
+                title: "Validation Error",
+                message: "Server Address is not a valid URL",
+                preferredStyle: .alert
+            )
+
             alert.addAction(UIAlertAction(title: "Confirm", style: .cancel, handler: nil))
             present(alert, animated: true, completion: nil)
             return
         }
+
         let secured = secureSwitch.isOn
         activityIndicator.startAnimating()
         RocketChat.configure(withServerURL: serverUrl, secured: secured) {
