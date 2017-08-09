d83345b718c1d6edad5abbe74e32eefcf6b9d4aa
Rafael Kellermann Streit <rafaelks@me.com>
2017-08-09 10:28:22 -0300

    Create a helper to create filename, data lenght and mimetype

---
+++ Rocket.Chat/Helpers/UploadHelper.swift
@@ -0,0 +1,9 @@
+//
+//  UploadHelper.swift
+//  Rocket.Chat
+//
+//  Created by Rafael Kellermann Streit on 09/08/17.
+//  Copyright © 2017 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
