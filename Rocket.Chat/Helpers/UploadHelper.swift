2e30b14a694554b38b371a1ba613e487535cd9ce
Rafael Kellermann Streit <rafaelks@me.com>
2017-08-09 10:28:30 -0300

    Support uploading different kind of files on iOS

--- Rocket.Chat/Helpers/UploadHelper.swift
+++ Rocket.Chat/Helpers/UploadHelper.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import MobileCoreServices
+
+struct UploadHelper {
+
+    static func file(for url: URL) -> FileUpload? {
+        guard let data = try? Data(contentsOf: url) else { return nil }
+
+        return FileUpload(
+            name: nameFor(url),
+            size: sizeFor(data),
+            type: mimeTypeFor(url),
+            data: data
+        )
+    }
+
+    static func sizeFor(_ data: Data) -> Int {
+        return (data as NSData).length
+    }
+
+    static func nameFor(_ url: URL) -> String {
+        return url.pathComponents.last ?? "Unknown.\(url.pathExtension)"
+    }
+
+    static func mimeTypeFor(_ url: URL) -> String {
+        let pathExtension = url.pathExtension
+
+        if let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, pathExtension as NSString, nil)?.takeRetainedValue() {
+            if let mimetype = UTTypeCopyPreferredTagWithClass(uti, kUTTagClassMIMEType)?.takeRetainedValue() {
+                return mimetype as String
+            }
+        }
+
+        return "application/octet-stream"
+    }
+
+}
