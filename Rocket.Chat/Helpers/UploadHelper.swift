a34e730a2a0870df3e73d0ab7af61aef91b78495
Rafael Kellermann Streit <rafaelks@me.com>
2017-08-09 11:40:42 -0300

    Add docs to UploadHelper

--- Rocket.Chat/Helpers/UploadHelper.swift
+++ Rocket.Chat/Helpers/UploadHelper.swift
@@ -11,6 +11,14 @@ import MobileCoreServices
 
 struct UploadHelper {
 
+    /**
+        This method will creates a FileUpload object
+        based on a file URL.
+ 
+        - parameters:
+            - url: The file URL to be based on.
+        - returns: FileUpload object, if URL is valid.
+     */
     static func file(for url: URL) -> FileUpload? {
         guard let data = try? Data(contentsOf: url) else { return nil }
 
@@ -22,14 +30,43 @@ struct UploadHelper {
         )
     }
 
+    /**
+        This method returns the size of some Data object.
+
+        - parameters:
+          - data: The data object to be inspected.
+        - returns: The size of the data
+     */
     static func sizeFor(_ data: Data) -> Int {
         return (data as NSData).length
     }
 
+    /**
+        This method tries to generate a name for a file with
+        an extension, based on the URL of it. For example:
+        - foo/bar.jpg will return bar.jpg
+        - foo/bar/baz.png will return baz.jpg
+        - foo will return foo
+
+        - parameters:
+            - url: The URL of the file to be generated the name.
+        - returns: Filename based on the URL.
+     */
     static func nameFor(_ url: URL) -> String {
         return url.pathComponents.last ?? "Unknown.\(url.pathExtension)"
     }
 
+    /**
+        This method tries to return the correct mimetype of
+        some file, based on the URL of it. For example:
+        - foo/bar.jpg will return image/jpg
+        - foo/bar/baz.pdf will return application/pdf
+        - foo will return application/octet-stream
+ 
+        - parameters:
+            - url: The URL of the file to get the mimetype.
+        - returns: the mimetype of the file.
+     */
     static func mimeTypeFor(_ url: URL) -> String {
         let pathExtension = url.pathExtension
 
