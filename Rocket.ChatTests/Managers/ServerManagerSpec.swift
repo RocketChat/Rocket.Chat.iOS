ad1bac1307057c7da414f595de2f757e4aa92864
Rafael Kellermann Streit <rafaelks@me.com>
2017-09-06 08:23:42 -0300

    Start writing tests to ServerManager

---
+++ Rocket.ChatTests/Managers/ServerManagerSpec.swift
@@ -0,0 +1,18 @@
+//
+//  ServerManagerSpec.swift
+//  Rocket.Chat
+//
+//  Created by Rafael Kellermann Streit on 05/09/17.
+//  Copyright © 2017 Rocket.Chat. All rights reserved.
+//
+
+import XCTest
+@testable import Rocket_Chat
+
+class ServerManagerSpec: XCTestCase {
+
+    func testUpdateServerInformation() {
+        
+    }
+    
+}
