d662bf619b7f69f82a84a56cf89ac75dba71a179
Rafael Kellermann Streit <rafaelks@me.com>
2017-09-12 09:43:45 -0300

    Implement a coordinator for network manager

---
+++ Rocket.Chat/Managers/Launcher/NetworkCoordinator.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkCoordinator.swift
+//  Rocket.Chat
+//
+//  Created by Rafael Kellermann Streit on 12/09/17.
+//  Copyright © 2017 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkCoordinator: LauncherProtocol {
+    func prepareToLaunch(with options: [UIApplicationLaunchOptionsKey: Any]?) {
+        NetworkManager.shared
+    }
+}
