62aeadf78a0d9905e4ad5242b179039454c5a764
Augusto Falcao <falcaoaugustos@gmail.com>
2017-09-15 10:58:35 -0300

    Ask user for review using iOS 10 API

---
+++ Rocket.Chat/UserReviewManager.swift
@@ -0,0 +1,50 @@
+//
+//  UserReviewManager.swift
+//  Rocket.Chat
+//
+//  Created by Augusto Falcão on 9/14/17.
+//  Copyright © 2017 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
+import StoreKit
+
+class UserReviewManager {
+    static let shared = UserReviewManager()
+
+    var nextDateForReview: Date = Date()
+    let nextDateForReviewKey: String = "kNextDateForReview"
+
+    var availableForReview: Bool {
+        return nextDateForReview < Date()
+    }
+
+    private let defaults = UserDefaults.standard
+
+    init() {
+        if let savedDate = defaults.object(forKey: nextDateForReviewKey) as? Date {
+            nextDateForReview = savedDate
+        } else {
+            setNextDateForReview()
+        }
+    }
+
+    func setNextDateForReview() {
+        let week: TimeInterval = 604800
+        nextDateForReview = Date().addingTimeInterval(week)
+
+        defaults.set(nextDateForReview, forKey: nextDateForReviewKey)
+    }
+
+    func requestReview() {
+        if availableForReview {
+            if #available(iOS 10.3, *) {
+                SKStoreReviewController.requestReview()
+            } else {
+                // Fallback on earlier versions
+            }
+
+            setNextDateForReview()
+        }
+    }
+}
