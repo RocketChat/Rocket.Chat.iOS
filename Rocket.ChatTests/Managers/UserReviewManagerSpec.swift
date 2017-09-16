8e33d9419f33ae64574d059de85ff8836d04d801
Augusto Falcao <falcaoaugustos@gmail.com>
2017-09-16 00:07:10 -0300

    Fix the week value and refactor this value to be a constant

--- Rocket.ChatTests/Managers/UserReviewManagerSpec.swift
+++ Rocket.ChatTests/Managers/UserReviewManagerSpec.swift
@@ -12,8 +12,7 @@ import XCTest
 class UserReviewManagerSpec: XCTestCase {
     func testCalculateNextDateForReview() {
         let manager = UserReviewManager()
-        let week: TimeInterval = 604800
-        let nextWeek = Date().addingTimeInterval(week)
+        let nextWeek = Date().addingTimeInterval(manager.week)
 
         XCTAssert(manager.calculateNextDateForReview().timeIntervalSince(nextWeek) < 0.1, "calculateNextDateForReview returns the value from next week in seconds")
     }
@@ -24,16 +23,14 @@ class UserReviewManagerSpec: XCTestCase {
     }
 
     func testRequestReviewSuccess() {
-        let week: TimeInterval = 604800
         let manager = UserReviewManager()
-        manager.nextDateForReview = Date(timeIntervalSince1970: week)
+        manager.nextDateForReview = Date(timeIntervalSince1970: manager.week)
         XCTAssert(manager.requestReview(), "manager requests user for review")
     }
 
     func testRequestReviewFailure() {
-        let week: TimeInterval = 604800
         let manager = UserReviewManager()
-        manager.nextDateForReview = Date(timeIntervalSinceNow: week)
+        manager.nextDateForReview = Date(timeIntervalSinceNow: manager.week)
         XCTAssertFalse(manager.requestReview(), "manager fails to request user for review")
     }
 }
