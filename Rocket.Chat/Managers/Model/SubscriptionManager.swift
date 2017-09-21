51b54b3a83b54245b08cc3a072717db367314b2a
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2017-09-21 00:32:10 -0300

    Send the notify on the room about "User is typing"

--- Rocket.Chat/Managers/Model/SubscriptionManager.swift
+++ Rocket.Chat/Managers/Model/SubscriptionManager.swift
@@ -164,13 +164,15 @@ struct SubscriptionManager {
         }
     }
 
+    // MARK: Typing
+
     static func subscribeTypingEvent(_ subscription: Subscription, completion: @escaping (String?, Bool) -> Void) {
         let eventName = "\(subscription.rid)/typing"
         let request = [
             "msg": "sub",
             "name": "stream-notify-room",
             "params": [eventName, false]
-            ] as [String : Any]
+        ] as [String : Any]
 
         SocketManager.subscribe(request, eventName: eventName) { response in
             guard !response.isError() else { return Log.debug(response.result.string) }
@@ -182,7 +184,23 @@ struct SubscriptionManager {
             completion(userNameTyping, flag)
         }
     }
-    
+
+    static func sendTypingStatus(_ subscription: Subscription, isTyping: Bool, completion: MessageCompletion? = nil) {
+        guard let username = AuthManager.currentUser()?.username else { return }
+
+        let request = [
+            "msg": "method",
+            "method": "stream-notify-room",
+            "params": ["\(subscription.rid)/typing", username, isTyping]
+        ] as [String : Any]
+
+        SocketManager.send(request) { response in
+            guard !response.isError() else { return Log.debug(response.result.string) }
+
+            completion?(response)
+        }
+    }
+
     // MARK: Search
 
     static func spotlight(_ text: String, completion: @escaping MessageCompletionObjectsList<Subscription>) {
