8dfb878dc457ebbd2298d94a227f7b0050b5cda7
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2017-09-28 08:54:11 -0300

    Add cell to input text

---
+++ Rocket.Chat/Views/Cells/NewChannel/NewChannelTextFieldCell.swift
@@ -0,0 +1,29 @@
+//
+//  NewChannelTextFieldCell.swift
+//  Rocket.Chat
+//
+//  Created by Bruno Macabeus Aquino on 28/09/17.
+//  Copyright © 2017 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
+
+class NewChannelTextFieldCell: UITableViewCell, NewChannelCellProtocol {
+    static let identifier = "kNewChannelTextField"
+    static let defaultHeight: Float = 68
+    weak var delegate: NewChannelCellDelegate?
+    var key: String?
+
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var textFieldInput: UITextField!
+
+    func setPreviousValue(previous: Any) {
+        if let previous = previous as? String {
+            textFieldInput.text = previous
+        }
+    }
+
+    @IBAction func textFieldInputEditingDidEnd(_ sender: Any) {
+        delegate?.updateDictValue(key: key ?? "", value: textFieldInput.text ?? "")
+    }
+}
