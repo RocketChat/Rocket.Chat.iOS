92f50766be718a1a9a09632925f58b937d675c98
cardoso <matheus@cardo.so>
2017-10-18 22:31:43 -0300

    Copy alert method to MainChatViewController for now

--- Rocket.Chat/Controllers/Chat/MainChatViewController.swift
+++ Rocket.Chat/Controllers/Chat/MainChatViewController.swift
@@ -129,11 +129,18 @@ extension MainChatViewController: SocketConnectionHandler {
     func socketDidReturnError(socket: SocketManager, error: SocketError) {
         switch error.error {
         case .invalidUser:
-            SubscriptionsViewController.shared?.alert(title: localized("alert.socket_error.invalid_user.title"), message: localized("alert.socket_error.invalid_user.message")) { _ in
+            alert(title: localized("alert.socket_error.invalid_user.title"),
+                  message: localized("alert.socket_error.invalid_user.message")) { _ in
                 self.logout()
             }
         default:
             break
         }
+    }
+
+    func alert(title: String, message: String, handler: ((UIAlertAction) -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: handler))
+        present(alert, animated: true, completion: nil)
     }
 }
