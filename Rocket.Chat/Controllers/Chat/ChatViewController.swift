cf849679e8541a1bf27829bb664621f39d6d44ef
Rafael Kellermann Streit <rafaelks@me.com>
2017-10-23 08:51:05 -0200

    Fixed: call append again if a message could not be added to the list

--- Rocket.Chat/Controllers/Chat/ChatViewController.swift
+++ Rocket.Chat/Controllers/Chat/ChatViewController.swift
@@ -75,7 +75,7 @@ final class ChatViewController: SLKTextViewController {
             markAsRead()
             typingIndicatorView?.dismissIndicator()
 
-            if let oldValue = oldValue, oldValue != subscription {
+            if let oldValue = oldValue, oldValue.identifier != subscription.identifier {
                 unsubscribe(for: oldValue)
             }
         }
@@ -412,14 +412,14 @@ final class ChatViewController: SLKTextViewController {
 
         dataController.loadedAllMessages = false
         isRequestingHistory = false
-        loadMoreMessagesFrom(date: nil)
         updateMessagesQueryNotificationBlock()
+        loadMoreMessagesFrom(date: nil)
 
         MessageManager.changes(subscription)
-        typingEvent()
+        registerTypingEvent()
     }
 
-    fileprivate func typingEvent() {
+    fileprivate func registerTypingEvent() {
         typingIndicatorView?.interval = 0
 
         SubscriptionManager.subscribeTypingEvent(subscription) { [weak self] username, flag in
@@ -448,10 +448,6 @@ final class ChatViewController: SLKTextViewController {
             }
 
             if insertions.count > 0 {
-                if insertions.count > 1 && self.isRequestingHistory {
-                    return
-                }
-
                 var newMessages: [Message] = []
                 for insertion in insertions {
                     let newMessage = Message(value: self.messagesQuery[insertion])
@@ -566,8 +562,16 @@ final class ChatViewController: SLKTextViewController {
     }
 
     fileprivate func appendMessages(messages: [Message], completion: VoidCompletion?) {
-        guard !isAppendingMessages else { return }
         guard let collectionView = self.collectionView else { return }
+        guard !isAppendingMessages else {
+            Log.debug("[APPEND MESSAGES] Blocked trying to append \(messages.count) messages")
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: { [weak self] in
+                self?.appendMessages(messages: messages, completion: completion)
+            })
+
+            return
+        }
 
         isAppendingMessages = true
 
