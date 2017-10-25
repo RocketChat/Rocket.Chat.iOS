c7f330d4d97a655b00db1f06cd6205fafaab62e5
cardoso <matheus@cardo.so>
2017-10-25 16:32:29 -0300

    Allow room owner to post in read only room

--- Rocket.Chat/Controllers/Chat/ChatViewController.swift
+++ Rocket.Chat/Controllers/Chat/ChatViewController.swift
@@ -402,7 +402,7 @@ final class ChatViewController: SLKTextViewController {
             showChatPreviewModeView()
         }
 
-        if subscription.roomReadOnly {
+        if subscription.roomReadOnly && subscription.roomOwner != AuthManager.currentUser() {
             blockMessageSending(reason: localized("chat.read_only"))
         } else {
             allowMessageSending()
@@ -638,7 +638,6 @@ final class ChatViewController: SLKTextViewController {
 
     fileprivate func blockMessageSending(reason: String) {
         textInputbar.textView.placeholder = reason
-        textInputbar.textView.placeholderColor = .black
         textInputbar.backgroundColor = .white
         textInputbar.isUserInteractionEnabled = false
         leftButton.isEnabled = false
@@ -647,7 +646,6 @@ final class ChatViewController: SLKTextViewController {
 
     fileprivate func allowMessageSending() {
         textInputbar.textView.placeholder = ""
-        textInputbar.textView.placeholderColor = .placeholderGray
         textInputbar.backgroundColor = .backgroundWhite
         textInputbar.isUserInteractionEnabled = true
         leftButton.isEnabled = true
