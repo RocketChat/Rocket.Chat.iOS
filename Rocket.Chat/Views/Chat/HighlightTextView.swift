f668860ea9b67a4a0199d73af8d9c724efd72a90
Artur Rymarz <artur.rymarz@finanteq.com>
2017-10-26 11:00:27 +0200

    fix highlight background positioning, increase size of lines when there is mention in message, clean code

--- Rocket.Chat/Views/Chat/HighlightTextView.swift
+++ Rocket.Chat/Views/Chat/HighlightTextView.swift
@@ -21,9 +21,10 @@ class HighlightTextView: UITextView {
             return
         }
 
+        let scrollViewInset: CGFloat = 5
         let framesetter: CTFramesetter = CTFramesetterCreateWithAttributedString(string)
         let path: CGMutablePath = CGMutablePath()
-        path.addRect(CGRect(x: 0, y: 0, width: bounds.size.width - 16, height: bounds.size.height))
+        path.addRect(CGRect(x: 0, y: 0, width: bounds.size.width - 2 * scrollViewInset, height: bounds.size.height))
         let totalframe: CTFrame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, 0), path, nil)
 
         guard let context: CGContext = UIGraphicsGetCurrentContext() else {
@@ -34,12 +35,10 @@ class HighlightTextView: UITextView {
         context.translateBy(x: 0, y: bounds.size.height)
         context.scaleBy(x: 1.0, y: -1.0)
 
-//        CTFrameDraw(totalframe, context)
-
         let lines = CTFrameGetLines(totalframe) as NSArray
 
         var origins = [CGPoint](repeating: .zero, count: lines.count)
-        CTFrameGetLineOrigins(totalframe, CFRangeMake(0, 0), &origins)
+        CTFrameGetLineOrigins(totalframe, CFRangeMake(0, lines.count), &origins)
 
         for index in 0..<lines.count {
             // swiftlint:disable force_cast
@@ -55,18 +54,17 @@ class HighlightTextView: UITextView {
 
                 let attributes = CTRunGetAttributes(run) as NSDictionary
 
-                //"highlightText"
-                if let color: UIColor = attributes.object(forKey: "highlightColor") as? UIColor {
+                if let color: UIColor = attributes.object(forKey: NSAttributedStringKey.highlightBackgroundColor) as? UIColor {
                     var runBounds: CGRect = .zero
                     var ascent: CGFloat = 0
                     var descent: CGFloat = 0
 
-                    runBounds.size.width = CGFloat(CTRunGetTypographicBounds(run, CFRangeMake(0, 0), &ascent, &descent, nil)) + 6
+                    runBounds.size.width = CGFloat(CTRunGetTypographicBounds(run, CFRangeMake(0, 0), &ascent, &descent, nil))
                     runBounds.size.height = ascent + descent
 
                     let xOffset = CTLineGetOffsetForStringIndex(line, CTRunGetStringRange(run).location, nil)
-                    runBounds.origin.x = origins[index].x + xOffset + 2
-                    runBounds.origin.y = origins[index].y - 4
+                    runBounds.origin.x = origins[index].x + xOffset + scrollViewInset
+                    runBounds.origin.y = origins[index].y - scrollViewInset
 
                     let path = UIBezierPath(roundedRect: runBounds, cornerRadius: 5)
 
