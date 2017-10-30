ea9dec2d19383698b0715cd396b3effa7f2c3c73
cardoso <matheus@cardo.so>
2017-10-30 16:26:37 -0300

    Show member count in members list only after loading

--- Rocket.Chat/Controllers/Chat/MembersListViewController.swift
+++ Rocket.Chat/Controllers/Chat/MembersListViewController.swift
@@ -17,9 +17,7 @@ class MembersListViewData {
     var showing: Int = 0
     var total: Int = 0
 
-    var title: String {
-        return String(format: localized("chat.members.list.title"), total)
-    }
+    var title: String = localized("chat.members.list.title")
 
     var isShowingAllMembers: Bool {
         return showing >= total
@@ -49,6 +47,7 @@ class MembersListViewData {
 
                 self.currentPage += 1
 
+                self.title = "\(localized("chat.members.list.title")) (\(self.total))"
                 self.isLoadingMoreMembers = false
                 completion?()
             }
