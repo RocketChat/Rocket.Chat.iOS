bd477db84915e617b788c3abc874c25dc41da3e7
filipealva <ofilipealvarenga@gmail.com>
2017-11-07 13:28:23 -0200

    Add documentation for DraftMessageManager

--- Rocket.Chat/Managers/DraftMessageManager.swift
+++ Rocket.Chat/Managers/DraftMessageManager.swift
@@ -11,19 +11,44 @@ import Foundation
 struct DraftMessageManager {
 
     static let userDefaults = UserDefaults.standard
+
+    /**
+         This property gets the selected server's URL for us to use it as a identification
+         to store and retrieve its subscriptions's draft messages. If the server URL
+         wind up changing the clients will need to logout and add a new server, since we clear
+         the cache when the user logouts, using the server URL will help us to avoid keeping invalid
+         server draft messages stored.
+     */
     static var selectedServerKey: String {
         return DatabaseManager.servers?[DatabaseManager.selectedIndex][ServerPersistKeys.serverURL] ?? ""
     }
 
+    /**
+         This method takes a subscription id and returns a more meaningful key
+         for us to persist.
+
+         - parameter identifier: The subscription id to update or retrieve a draft message.
+     */
     static internal func draftMessageKey(for identifier: String) -> String {
         return String(format: "\(identifier)-cacheddraftmessage")
     }
 
+    /**
+         This method clears all the cached draft messages for the selected server if any.
+     */
     static func clearServerDraftMessages() {
         guard !selectedServerKey.isEmpty else { return }
         userDefaults.set(nil, forKey: selectedServerKey)
     }
 
+    /**
+         This method takes a draft message and a subscription so it can update
+         the subscription current draft message then when the user come back to
+         this subscription in the app the draft message will be available for him.
+
+         - parameter draftMessage: The new draft message to update.
+         - parameter subscription: The subscription that is related to the draftMessage.
+     */
     static func update(draftMessage: String, for subscription: Subscription) {
         guard !selectedServerKey.isEmpty else { return }
         let subscriptionKey = draftMessageKey(for: subscription.rid)
@@ -36,6 +61,12 @@ struct DraftMessageManager {
         }
     }
 
+    /**
+         This method takes a subscription and returns the current cached draft message
+         that is related to it.
+
+         - parameter subscription: The subscription we that we need the draft message.
+     */
     static func draftMessage(for subscription: Subscription) -> String {
         guard !selectedServerKey.isEmpty else { return "" }
         let subscriptionKey = draftMessageKey(for: subscription.rid)
