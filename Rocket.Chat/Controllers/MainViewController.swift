c31cba49bbc8c67b8b873d79a8aeb167445cae23
Rafael Kellermann Streit <rafaelks@me.com>
2017-11-17 06:47:00 -0200

    Make a few changes on handling the redirect after server exists

--- Rocket.Chat/Controllers/MainViewController.swift
+++ Rocket.Chat/Controllers/MainViewController.swift
@@ -13,6 +13,8 @@ final class MainViewController: BaseViewController {
     @IBOutlet weak var labelAuthenticationStatus: UILabel!
     @IBOutlet weak var buttonConnect: UIButton!
 
+    let infoRequestHandler = InfoRequestHandler()
+
     var activityIndicator: LoaderView!
     @IBOutlet weak var activityIndicatorContainer: UIView! {
         didSet {
@@ -28,6 +30,9 @@ final class MainViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         AuthManager.recoverAuthIfNeeded()
+
+        infoRequestHandler.delegate = self
+        infoRequestHandler.validateServerVersion = false
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -46,37 +51,7 @@ final class MainViewController: BaseViewController {
 
         if let auth = AuthManager.isAuthenticated() {
             AuthManager.persistAuthInformation(auth)
-
-            AuthManager.resume(auth, completion: { [weak self] response in
-                guard !response.isError() else {
-                    self?.labelAuthenticationStatus.isHidden = false
-                    self?.buttonConnect.isHidden = false
-                    self?.activityIndicator.stopAnimating()
-
-                    self?.openChat()
-
-                    return
-                }
-
-                SubscriptionManager.updateSubscriptions(auth, completion: { _ in
-                    AuthSettingsManager.updatePublicSettings(auth, completion: { _ in
-
-                    })
-
-                    UserManager.userDataChanges()
-                    UserManager.changes()
-                    SubscriptionManager.changes(auth)
-                    SubscriptionManager.subscribeRoomChanges()
-                    PermissionManager.changes()
-                    PermissionManager.updatePermissions()
-
-                    if let userIdentifier = auth.userId {
-                        PushManager.updateUser(userIdentifier)
-                    }
-
-                    self?.openChat()
-                })
-            })
+            infoRequestHandler.validate()
         } else {
             let storyboardAuth = UIStoryboard(name: "Auth", bundle: Bundle.main)
             let controller = storyboardAuth.instantiateInitialViewController()
@@ -95,6 +70,78 @@ final class MainViewController: BaseViewController {
 
         if let window = application.keyWindow {
             window.rootViewController = controller
+        }
+    }
+
+    func resumeAuth() {
+        guard let auth = AuthManager.isAuthenticated() else { return }
+
+        AuthManager.resume(auth, completion: { [weak self] response in
+            guard !response.isError() else {
+                self?.labelAuthenticationStatus.isHidden = false
+                self?.buttonConnect.isHidden = false
+                self?.activityIndicator.stopAnimating()
+
+                self?.openChat()
+
+                return
+            }
+
+            SubscriptionManager.updateSubscriptions(auth, completion: { _ in
+                AuthSettingsManager.updatePublicSettings(auth, completion: { _ in
+
+                })
+
+                UserManager.userDataChanges()
+                UserManager.changes()
+                SubscriptionManager.changes(auth)
+                SubscriptionManager.subscribeRoomChanges()
+                PermissionManager.changes()
+                PermissionManager.updatePermissions()
+
+                if let userIdentifier = auth.userId {
+                    PushManager.updateUser(userIdentifier)
+                }
+
+                self?.openChat()
+            })
+        })
+    }
+
+}
+
+extension MainViewController: InfoRequestHandlerDelegate {
+
+    var viewControllerToPresentAlerts: UIViewController? { return self }
+
+    func urlNotValid() {
+        // Do nothing
+    }
+
+    func serverIsValid() {
+        DispatchQueue.main.async {
+            self.resumeAuth()
+        }
+    }
+
+    func serverChangedURL(_ newURL: String?) {
+        if let url = newURL {
+            if let socketURL = URL(string: url)?.socketURL() {
+                let newIndex = DatabaseManager.copyServerInformation(
+                    from: DatabaseManager.selectedIndex,
+                    with: socketURL.absoluteString
+                )
+
+                DatabaseManager.selectDatabase(at: newIndex)
+                DatabaseManager.cleanInvalidDatabases()
+                DatabaseManager.changeDatabaseInstance()
+
+                DispatchQueue.main.async {
+                    self.infoRequestHandler.validate()
+                }
+            }
+        } else {
+            self.resumeAuth()
         }
     }
 
