b5aa4cd6e9e648685d5ff9f9b9570f2e97d17245
Rafael Kellermann Streit <rafaelks@me.com>
2017-11-24 11:15:32 -0200

    Update file to use scripts

--- Scripts/update_dependencies.sh
+++ Scripts/update_dependencies.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# Gems & Swiftlint
+gem install bundler
+gem install xcpretty
+gem install cocoapods
+brew install swiftlint
+
 # Update all external dependencies
-pod repo update
+curl https://cocoapods-specs.circleci.com/fetch-cocoapods-repo-from-s3.sh | bash -s cf
 pod install
