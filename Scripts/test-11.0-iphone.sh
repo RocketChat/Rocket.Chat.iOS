cbd2bddc3f29ad570b082e5c43ce8c1dd7d52294
Rafael Kellermann Streit <rafaelks@me.com>
2017-11-24 12:07:49 -0200

    Run codecov after iPhone 11 tests

--- Scripts/test-11.0-iphone.sh
+++ Scripts/test-11.0-iphone.sh
@@ -5,3 +5,5 @@ xcodebuild clean test \
     -scheme Rocket.Chat \
     -destination "platform=iOS Simulator,OS=11.0,name=iPhone 8" \
     -enableCodeCoverage YES | xcpretty -c && exit ${PIPESTATUS[0]}
+
+bash <(curl -s https://codecov.io/bash) -J 'Rocket.Chat'
