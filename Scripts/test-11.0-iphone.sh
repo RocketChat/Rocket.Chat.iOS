14eba253c909d61cf478d65e4d70601e54694d49
Rafael Kellermann Streit <rafaelks@me.com>
2017-11-24 13:05:26 -0200

    Add codecov to Circleci

--- Scripts/test-11.0-iphone.sh
+++ Scripts/test-11.0-iphone.sh
@@ -5,5 +5,3 @@ xcodebuild clean test \
     -scheme Rocket.Chat \
     -destination "platform=iOS Simulator,OS=11.0,name=iPhone 8" \
     -enableCodeCoverage YES | xcpretty -c && exit ${PIPESTATUS[0]}
-
-bash <(curl -s https://codecov.io/bash) -J 'Rocket.Chat'
