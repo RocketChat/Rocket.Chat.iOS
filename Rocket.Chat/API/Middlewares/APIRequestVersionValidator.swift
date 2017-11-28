fbcaef60b2bbb1800ffc86e51d515b1befd63d1d
cardoso <matheus@cardo.so>
2017-11-28 14:49:03 -0300

    Fix mistake in version

---
+++ Rocket.Chat/API/Middlewares/APIRequestVersionValidator.swift
@@ -0,0 +1,19 @@
+//
+//  APIRequestVersionValidator.swift
+//  Rocket.Chat
+//
+//  Created by Matheus Cardoso on 11/28/17.
+//  Copyright © 2017 Rocket.Chat. All rights reserved.
+//
+
+struct APIRequestVersionValidator: APIRequestMiddleware {
+    let api: API
+
+    func handle<R: APIRequest>(_ request: inout R) -> APIError? {
+        if api.version < request.requiredVersion {
+            return APIError.version(available: api.version, required: request.requiredVersion)
+        }
+
+        return nil
+    }
+}
