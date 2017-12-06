c9e088349db4b5e2d4c0fac5e7a25b5be9b24b54
Rafael Kellermann Streit <rafaelks@me.com>
2017-12-06 07:52:43 -0200

    Add an activity indicator to the webview

--- Rocket.Chat/Controllers/WebViewEmbedded/WebViewControllerEmbedded.swift
+++ Rocket.Chat/Controllers/WebViewEmbedded/WebViewControllerEmbedded.swift
@@ -11,21 +11,36 @@ import WebKit
 
 class WebViewControllerEmbedded: UIViewController {
 
+    var urlLoaded = false
     var url: URL?
     weak var webView: WKWebView!
+
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.whiteLarge)
+        activityIndicator.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+        activityIndicator.layer.cornerRadius = 10
+        activityIndicator.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        activityIndicator.center = view.center
+        view.addSubview(activityIndicator)
+        return activityIndicator
+    }()
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        if let url = url {
-            let request = URLRequest(url: url)
+        if !urlLoaded {
+            if let url = url {
+                let request = URLRequest(url: url)
 
-            let webView = WKWebView(frame: view.frame)
-            webView.navigationDelegate = self
-            webView.load(request)
-            view.addSubview(webView)
+                let webView = WKWebView(frame: view.frame)
+                webView.navigationDelegate = self
+                webView.load(request)
+                view.addSubview(webView)
 
-            self.webView = webView
+                self.webView = webView
+                self.urlLoaded = true
+                self.activityIndicator.startAnimating()
+            }
         }
     }
 
@@ -33,7 +48,15 @@ class WebViewControllerEmbedded: UIViewController {
 
 extension WebViewControllerEmbedded: WKNavigationDelegate {
 
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        activityIndicator.stopAnimating()
+        activityIndicator.removeFromSuperview()
+    }
+
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        activityIndicator.stopAnimating()
+        activityIndicator.removeFromSuperview()
+
         let token = AuthManager.isAuthenticated()?.token ?? ""
 
         //swiftlint:disable
