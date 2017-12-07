c25b7b2852c4bdcc685014ce321ad2267cc166e7
cardoso <matheus@cardo.so>
2017-12-07 10:26:45 -0300

    Add id parameter to sendMessage

--- Rocket.ChatTests/API/Clients/MessagesClientSpec.swift
+++ Rocket.ChatTests/API/Clients/MessagesClientSpec.swift
@@ -38,48 +38,7 @@ class MessagesClientSpec: XCTestCase, RealmTestCase {
         let user = User.testInstance()
         let subscription = Subscription.testInstance()
 
-        client.sendMessage(text: "test", subscription: subscription, user: user, realm: realm)
-
-        let messages = realm.objects(Message.self)
-        XCTAssertEqual(messages.count, 1)
-        XCTAssertEqual(realm.objects(Message.self).first?.temporary, true)
-
-        let expectation = XCTestExpectation(description: "message updated in realm")
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: {
-            if realm.objects(Message.self).first?.temporary == false {
-                expectation.fulfill()
-            }
-        })
-        wait(for: [expectation], timeout: 1.1)
-    }
-
-    func testSendMessageFallback() {
-        let api = MockAPI()
-        let realm = testRealm()
-        let client = MessagesClient(api: api)
-
-        api.nextResult = JSON([
-            "success": true,
-            "message": [
-                "mentions": [],
-                "_id": "a43SYFpMdjEAdM0mrH",
-                "_updatedAt": "2017-12-07T12:30:38.384Z",
-                "channels": [],
-                "rid": "GENERAL",
-                "u": [
-                    "name": "Matheus Cardoso",
-                    "username": "matheus.cardoso",
-                    "_id": "ERoZg2xpgcDnXbCJu"
-                ],
-                "ts": "2017-12-07T12:30:38.382Z",
-                "msg": "Test"
-            ]
-            ])
-
-        let user = User.testInstance()
-        let subscription = Subscription.testInstance()
-
-        client.sendMessage(text: "test", subscription: subscription, user: user, realm: realm)
+        client.sendMessage(text: "test", subscription: subscription, id: "a43SYFpMdjEAdM0mrH", user: user, realm: realm)
 
         let messages = realm.objects(Message.self)
         XCTAssertEqual(messages.count, 1)
