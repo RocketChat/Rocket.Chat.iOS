6dd8e12990dd366777a5d6f36dbd28856298a3a2
cardoso <matheus@cardo.so>
2017-12-20 16:31:57 -0300

    Add EmojiPicker & EmojiPickerController

---
+++ Rocket.Chat/Views/EmojiPicker/EmojiPickerController.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiPickerController.swift
+//  Rocket.Chat
+//
+//  Created by Matheus Cardoso on 12/20/17.
+//  Copyright © 2017 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+
+class EmojiPickerController: UIViewController {
+    var emojiPicker: EmojiPicker! = nil
+
+    override func loadView() {
+        super.loadView()
+        emojiPicker = EmojiPicker()
+        view.addSubview(emojiPicker)
+
+        view.addConstraints(
+            NSLayoutConstraint.constraints(
+                withVisualFormat: "|-0-[view]-0-|", options: [], metrics: nil, views: ["view": emojiPicker]
+            )
+        )
+        view.addConstraints(
+            NSLayoutConstraint.constraints(
+                withVisualFormat: "V:|-0-[view]-0-|", options: [], metrics: nil, views: ["view": emojiPicker]
+            )
+        )
+    }
+}
