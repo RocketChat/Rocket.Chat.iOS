00d4674d545af4de10207fd6985e8cfe4c5912fb
Rafael Kellermann Streit <rafaelks@me.com>
2018-01-09 09:31:53 -0500

    Integrate with Instabug

--- Rocket.Chat/Managers/Launcher/BugTrackingCoordinator.swift
+++ Rocket.Chat/Managers/Launcher/BugTrackingCoordinator.swift
@@ -9,10 +9,24 @@
 import Foundation
 import Fabric
 import Crashlytics
+import Instabug
 
 struct BugTrackingCoordinator: LauncherProtocol {
     func prepareToLaunch(with options: [UIApplicationLaunchOptionsKey: Any]?) {
         launchFabric()
+        launchInstabug()
+    }
+
+    private func launchInstabug() {
+        guard
+            let path = Bundle.main.path(forResource: "Keys", ofType: "plist"),
+            let keys = NSDictionary(contentsOfFile: path),
+            let instabug = keys["Instabug"] as? String
+        else {
+            return
+        }
+
+        Instabug.start(withToken: instabug, invocationEvent: .floatingButton)
     }
 
     private func launchFabric() {
