8f3956f5ed6a506e83dfe333e78151b902bd2973
cardoso <matheus@cardo.so>
2018-01-11 07:26:15 -0300

    Avatar container corner radius = 4

--- Rocket.Chat/Views/Cells/Chat/ChatMessageCell.swift
+++ Rocket.Chat/Views/Cells/Chat/ChatMessageCell.swift
@@ -43,6 +43,7 @@ final class ChatMessageCell: UICollectionViewCell {
 
     @IBOutlet weak var avatarViewContainer: UIView! {
         didSet {
+            avatarViewContainer.layer.cornerRadius = 4
             if let avatarView = AvatarView.instantiateFromNib() {
                 avatarView.frame = avatarViewContainer.bounds
                 avatarViewContainer.addSubview(avatarView)
