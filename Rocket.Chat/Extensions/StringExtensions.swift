cdfe3b422fd384eba624db39000977cecceb4540
Matheus Cardoso <matheus@cardo.so>
2018-01-17 18:48:44 -0300

    Fix code style

--- Rocket.Chat/Extensions/StringExtensions.swift
+++ Rocket.Chat/Extensions/StringExtensions.swift
@@ -15,7 +15,7 @@ func localized(_ string: String) -> String {
 extension String {
     var isValidEmail: Bool {
         let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailFormat)
         return emailPredicate.evaluate(with: self)
     }
 
