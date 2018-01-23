259ca230914cf8919a8b869c09190e999d797f7a
Luca <git@sndstrm.de>
2018-01-23 14:34:30 +0100

    Improve coverage; reduce ABC by refactoring

--- Rocket.Chat/Views/Cells/Chat/ChatMessageImageView.swift
+++ Rocket.Chat/Views/Cells/Chat/ChatMessageImageView.swift
@@ -17,6 +17,7 @@ protocol ChatMessageImageViewProtocol: class {
 final class ChatMessageImageView: UIView {
     static let defaultHeight = CGFloat(250)
     var isLoadable = true
+    var placeholderImage: UIImage?
 
     weak var delegate: ChatMessageImageViewProtocol?
     var attachment: Attachment! {
@@ -44,6 +45,14 @@ final class ChatMessageImageView: UIView {
         return UITapGestureRecognizer(target: self, action: #selector(didTapView))
     }()
 
+    fileprivate func checkHttpImage(imageURL: URL) {
+        if imageURL.absoluteString.starts(with: "http://") {
+            isLoadable = false
+            labelTitle.text = attachment.title + " (" + localized("alert.insecure_image.title") + ")"
+            placeholderImage = UIImage(named: "Insecure Image")
+        }
+    }
+
     fileprivate func updateMessageInformation() {
         let containsGesture = gestureRecognizers?.contains(tapGesture) ?? false
         if !containsGesture {
@@ -53,14 +62,10 @@ final class ChatMessageImageView: UIView {
         guard let imageURL = attachment.fullImageURL() else {
             return
         }
-        var placeholderImage: UIImage?
-        if imageURL.absoluteString.starts(with: "http://") {
-            isLoadable = false
-            labelTitle.text = attachment.title + " (" + localized("alert.insecure_image.title") + ")"
-            placeholderImage = UIImage(named: "Insecure Image")
-        } else {
-            labelTitle.text = attachment.title
-        }
+
+        labelTitle.text = attachment.title
+        checkHttpImage(imageURL: imageURL)
+
         activityIndicatorImageView.startAnimating()
         imageView.sd_setImage(with: imageURL, placeholderImage: placeholderImage, completed: { [weak self] _, _, _, _ in
             self?.activityIndicatorImageView.stopAnimating()
