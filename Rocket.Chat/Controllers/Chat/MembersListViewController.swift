6940e24863ae9a6ce708d399ee9cce486f36842f
cardoso <matheus@cardo.so>
2018-02-08 21:42:15 -0300

    Open DM on select user in Members List

--- Rocket.Chat/Controllers/Chat/MembersListViewController.swift
+++ Rocket.Chat/Controllers/Chat/MembersListViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+protocol MembersListDelegate {
+    func membersList(_ controller: MembersListViewController, didSelectUser user: User)
+}
+
 class MembersListViewData {
     var subscription: Subscription?
 
@@ -66,6 +70,8 @@ class MembersListViewController: BaseViewController {
     var loaderCell: LoaderTableViewCell!
 
     var data = MembersListViewData()
+
+    var delegate: MembersListDelegate?
 
     @objc func refreshControlDidPull(_ sender: UIRefreshControl) {
         let data = MembersListViewData()
@@ -174,7 +180,10 @@ extension MembersListViewController: UITableViewDataSource {
 
 extension MembersListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: false)
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let user = data.member(at: indexPath.row)
+        delegate?.membersList(self, didSelectUser: user)
     }
 
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
