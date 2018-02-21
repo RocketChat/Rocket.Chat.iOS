1c517187dfbfe002aa902ad24e7249ea85e2ef67
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2018-02-21 10:00:19 -0300

    Speed up the tests even more

--- Scripts/test-clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-xcodebuild clean test \
-    -workspace Rocket.Chat.xcworkspace \
-    -scheme Rocket.Chat \
-    -destination "platform=iOS Simulator,OS=$1,name=$2" | xcpretty -c && exit ${PIPESTATUS[0]}
--- Scripts/test_without_building.sh
+++ Scripts/test_without_building.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-xcodebuild test \
+xcodebuild test-without-building \
     -workspace Rocket.Chat.xcworkspace \
     -scheme Rocket.Chat \
     -destination "platform=iOS Simulator,OS=$1,name=$2" \
-    -enableCodeCoverage YES | xcpretty -c && exit ${PIPESTATUS[0]}
+	-enableCodeCoverage YES | xcpretty -c && exit ${PIPESTATUS[0]}
