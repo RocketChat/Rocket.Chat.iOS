77deb2622260689ba1f73e04c04e3ce6da48b19a
anirbunt <anirbunt@gmail.com>
2018-02-26 08:43:54 +0530

    [NEW] added drawing feature

--- Rocket.Chat/Controllers/Chat/ChatViewController.swift
+++ Rocket.Chat/Controllers/Chat/ChatViewController.swift
@@ -14,6 +14,7 @@ import SimpleImageViewer
 final class ChatViewController: SLKTextViewController {
 
     var activityIndicator: LoaderView!
+    static var toUploadPic: UIImage!
     @IBOutlet weak var activityIndicatorContainer: UIView! {
         didSet {
             let width = activityIndicatorContainer.bounds.width
