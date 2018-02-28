f992a724d0698559a00089ef43cc4830df019508
cardoso <matheus@cardo.so>
2018-02-28 12:47:08 -0300

    Add Share Extension Target + ViewController

---
+++ Rocket.Chat.ShareExtension/ShareViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.swift
+//  Rocket.Chat.ShareExtension
+//
+//  Created by Matheus Cardoso on 2/28/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+import Social
+
+class ShareViewController: UIViewController {
+
+}
