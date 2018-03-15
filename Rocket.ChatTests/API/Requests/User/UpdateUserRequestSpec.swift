b0b68e46b5563e936a44b4b5394400ed95410e8f
filipealva <ofilipealvarenga@gmail.com>
2018-03-15 11:10:58 -0300

    Update UpdateUserRequestSpec

--- Rocket.ChatTests/API/Requests/User/UpdateUserRequestSpec.swift
+++ Rocket.ChatTests/API/Requests/User/UpdateUserRequestSpec.swift
@@ -15,12 +15,13 @@ class UpdateUserRequestSpec: APITestCase {
 
     func testRequest() {
         let password = "123456"
+        let currentPassword = "654321"
         let user = User()
         user.name = "Example User"
         user.username = "example"
         user.emails.append(Email(value: ["email": "example@example.com", "verified": true]))
 
-        let _request = UpdateUserRequest(user: user, password: password)
+        let _request = UpdateUserRequest(user: user, password: password, currentPassword: currentPassword)
 
         guard let request = _request.request(for: api) else {
             return XCTFail("request is not nil")
@@ -38,7 +39,8 @@ class UpdateUserRequestSpec: APITestCase {
         XCTAssertEqual(bodyJson["data"]["name"].string, user.name, "name is correct")
         XCTAssertEqual(bodyJson["data"]["username"].string, user.username, "username is correct")
         XCTAssertEqual(bodyJson["data"]["email"].string, user.emails.first?.email, "email is correct")
-        XCTAssertEqual(bodyJson["data"]["password"].string, password, "password is correct")
+        XCTAssertEqual(bodyJson["data"]["newPassword"].string, password, "password is correct")
+        XCTAssertEqual(bodyJson["data"]["currentPassword"].string, currentPassword.sha256(), "current password is correct")
     }
 
     func testResult() {
