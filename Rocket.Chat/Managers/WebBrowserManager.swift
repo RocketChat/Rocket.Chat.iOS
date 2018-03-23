7299906c912f5f6b1a3ad0e424b3feb9727bed0c
filipealva <ofilipealvarenga@gmail.com>
2018-03-23 11:30:17 -0300

    Add WebBrowserManager docs

--- Rocket.Chat/Managers/WebBrowserManager.swift
+++ Rocket.Chat/Managers/WebBrowserManager.swift
@@ -12,6 +12,11 @@ import SafariServices
 struct WebBrowserManager {
     private static let defaults = UserDefaults.standard
     private static let defaultBrowserKey = "DefaultBrowserKey"
+
+    /**
+        Returns the default browser the user has choosen to open any links in
+        Rocket Chat. If none was choosen the default option is set to .inAppSafari.
+     */
     static var browser: WebBrowserApp {
         guard
             let browserRaw = UserDefaults.standard.string(forKey: defaultBrowserKey),
@@ -28,14 +33,28 @@ struct WebBrowserManager {
         return browser
     }
 
+    /**
+        Open the given URL using the default browser the user has choosen.
+
+        - parameter url: Any URL that can be opened using a web browser app.
+     */
     static func open(url: URL) {
         browser.open(url: url)
     }
 
+    /**
+        Set the given web browser app as the default option
+        to open any links in Rocket Chat.
+
+        - parameter defaultBrowser: One of WebBrowserApp member values.
+     */
     static func set(defaultBrowser: WebBrowserApp) {
         defaults.set(defaultBrowser.rawValue, forKey: defaultBrowserKey)
     }
 
+    /**
+        Clears the current selected default browser
+     */
     static func clearDefaultBrowser() {
         defaults.set(nil, forKey: defaultBrowserKey)
     }
@@ -79,7 +98,7 @@ enum WebBrowserApp: String {
 }
 
 extension WebBrowserApp {
-    func appSchemeURL(forURL url: URL) -> URL? {
+    private func appSchemeURL(forURL url: URL) -> URL? {
         let scheme = url.scheme ?? ""
         var absoluteString = url.absoluteString
 
