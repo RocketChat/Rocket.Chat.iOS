c682a593e29ee5e33cc03482d7e0bb777339f697
filipealva <ofilipealvarenga@gmail.com>
2018-03-27 16:48:36 -0300

    Implement static functions to identify and remove identification from crash reports

--- Rocket.Chat/Managers/Launcher/BugTrackingCoordinator.swift
+++ Rocket.Chat/Managers/Launcher/BugTrackingCoordinator.swift
@@ -35,5 +35,39 @@ struct BugTrackingCoordinator: LauncherProtocol {
 
     private func launchFabric() {
         Fabric.with([Crashlytics.self])
+
+        if let currentUser = AuthManager.currentUser() {
+            BugTrackingCoordinator.identifyCrashReports(withUser: currentUser)
+        }
+    }
+
+    static func identifyCrashReports(withUser user: User) {
+        guard let id = user.identifier else {
+            return
+        }
+
+        let crashlytics = Crashlytics.sharedInstance()
+        crashlytics.setUserIdentifier(id)
+
+        if let name = user.name {
+            crashlytics.setUserName(name)
+        }
+
+        if let email = user.emails.first?.email {
+            crashlytics.setUserEmail(email)
+        }
+
+        if let serverURL = AuthManager.selectedServerInformation()?[ServerPersistKeys.serverURL] {
+            crashlytics.setValue(serverURL, forKey: ServerPersistKeys.serverURL)
+        }
+    }
+
+    static func removeUserFromCrashReports() {
+        let crashlytics = Crashlytics.sharedInstance()
+
+        crashlytics.setUserEmail(nil)
+        crashlytics.setUserName(nil)
+        crashlytics.setUserIdentifier(nil)
+        crashlytics.setValue(nil, forKey: ServerPersistKeys.serverURL)
     }
 }
