03c778a09de67d7eec09ca2c2580ab79ab47f4c9
filipealva <ofilipealvarenga@gmail.com>
2018-03-27 13:32:14 -0300

    Add EditProfileTableViewControllerSpec

--- Rocket.ChatTests/Controllers/EditProfileTableViewControllerSpec.swift
+++ Rocket.ChatTests/Controllers/EditProfileTableViewControllerSpec.swift
@@ -15,29 +15,65 @@ class EditProfileTableViewControllerSpec: XCTestCase {
 
     let editProfile = EditProfileTableViewController()
 
-    func test() {
-        let jsonString = """
-        {
-            "_id": "aobEdbYhXfu5hkeqG",
-            "name": "Example User",
-            "emails": [
-                {
-                    "address": "example@example.com",
-                    "verified": true
-                }
-            ],
-            "status": "offline",
-            "statusConnection": "offline",
-            "username": "example",
-            "utcOffset": 0,
-            "active": true,
-            "success": true
-        }
-        """
+    func testCanEditProfileOnly() {
+        let authSettings = AuthSettings()
+        authSettings.isAllowedToEditProfile = true
+        authSettings.isAllowedToEditAvatar = false
+        authSettings.isAllowedToEditName = false
+        authSettings.isAllowedToEditUsername = false
+        authSettings.isAllowedToEditEmail = false
+        authSettings.isAllowedToEditName = false
+        authSettings.isAllowedToEditPassword = false
 
-        let json = JSON(parseJSON: jsonString)
+        AuthSettingsManager.shared.internalSettings = authSettings
 
-        let result = MeResult(raw: json)
+        XCTAssertEqual(editProfile.canEditAnyInfo, false, "will show edit state without being able to edit anything.")
+        XCTAssertEqual(editProfile.numberOfSections, 1, "will show change password section without being able to update it.")
+    }
+
+    func testCanEditAnyInfo() {
+        let authSettings = AuthSettings()
+        authSettings.isAllowedToEditProfile = true
+        authSettings.isAllowedToEditAvatar = true
+
+        AuthSettingsManager.shared.internalSettings = authSettings
+
+        XCTAssertEqual(editProfile.canEditAnyInfo, true, "will not show edit state even though being able to edit avatar.")
+    }
+
+    func testNumberOfRows() {
+        let authSettings = AuthSettings()
+        authSettings.isAllowedToEditProfile = true
+        authSettings.isAllowedToEditPassword = true
+
+        AuthSettingsManager.shared.internalSettings = authSettings
+
+        editProfile.isLoading = true
+        XCTAssertEqual(editProfile.numberOfSections, 0, "will show sections even though it is in loading state.")
+
+        editProfile.isLoading = false
+        XCTAssertEqual(editProfile.numberOfSections, 2, "will not show password section even though it is allowed to edit password.")
+
+        authSettings.isAllowedToEditPassword = false
+        XCTAssertEqual(editProfile.numberOfSections, 1, "will show change password section without being able to update it.")
+    }
+
+    func testEditingState() {
+        let authSettings = AuthSettings()
+        authSettings.isAllowedToEditProfile = true
+        authSettings.isAllowedToEditAvatar = true
+        authSettings.isAllowedToEditName = true
+        authSettings.isAllowedToEditUsername = true
+        authSettings.isAllowedToEditEmail = true
+        authSettings.isAllowedToEditName = true
+
+        AuthSettingsManager.shared.internalSettings = authSettings
+        editProfile.enableUserInteraction()
+
+        XCTAssertEqual(editProfile.avatarButton.isEnabled, true, "avatar button is not enabled even though it is allowed to edit avatar.")
+        XCTAssertEqual(editProfile.name.isEnabled, true, "name field is not enabled even though it is allowed to edit name.")
+        XCTAssertEqual(editProfile.username.isEnabled, true, "username field is not enabled even though it is allowed to edit username.")
+        XCTAssertEqual(editProfile.email.isEnabled, true, "email field is not enabled even though it is allowed to edit email.")
     }
 
 }
