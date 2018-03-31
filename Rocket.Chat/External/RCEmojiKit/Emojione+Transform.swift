81bf4442f174a6d5c121862abd68f19ac01e4189
Samar Sunkaria <sameesunkaria@me.com>
2018-03-31 21:17:35 +0530

    Revert "Removed the old implementation of transform"
    This reverts commit 1338434218b24e157d5f97fb075dd1bbf112f7be.

--- Rocket.Chat/External/RCEmojiKit/Emojione+Transform.swift
+++ Rocket.Chat/External/RCEmojiKit/Emojione+Transform.swift
@@ -9,6 +9,34 @@
 import Foundation
 
 extension Emojione {
+    static func transrform(string: String) -> String {
+        var _string = string as NSString
+
+        let regex = try? NSRegularExpression(pattern: Emojione.regex, options: [])
+        let ranges = regex?.matches(
+            in: string,
+            options: [],
+            range: NSRange(location: 0, length: string.count)
+        ).map {
+            $0.range(at: 0)
+        } ?? []
+
+        // exclude matches inside code tags
+        let filteredRanges = string.filterOutRangesInsideCode(ranges: ranges)
+
+        var offset = 0
+        for range in filteredRanges {
+            let transformedRange = NSRange(location: range.location - offset, length: range.length)
+            let replacementString = _string.substring(with: transformedRange) as NSString
+
+            if let emoji = values[replacementString.replacingOccurrences(of: ":", with: "")] {
+                _string = _string.replacingCharacters(in: transformedRange, with: emoji) as NSString
+                offset += replacementString.length - (emoji as NSString).length
+            }
+        }
+
+        return _string as String
+    }
 
     static func transform(string: String) -> String {
         var _string = string as NSString
