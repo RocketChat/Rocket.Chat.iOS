cc5c2eaecafd8edb376b74f419e79a5bf6bc477f
Samar Sunkaria <sameesunkaria@me.com>
2018-03-31 21:35:00 +0530

    Updated to Swift 4.1

--- Rocket.Chat/Controllers/Auth/ConnectServerViewController.swift
+++ Rocket.Chat/Controllers/Auth/ConnectServerViewController.swift
@@ -133,7 +133,7 @@ final class ConnectServerViewController: BaseViewController {
 
     func connect() {
         guard let url = url else { return infoRequestHandler.alertInvalidURL() }
-        guard let socketURL = url.socketURL() else { return infoRequestHandler.alertInvalidURL() }
+        guard url.socketURL() != nil else { return infoRequestHandler.alertInvalidURL() }
 
         connecting = true
         textFieldServerURL.alpha = 0.5
