f7c490163bd2233ad15c8929760d2f66cd9c6d28
Samar Sunkaria <sameesunkaria@me.com>
2018-04-03 16:49:43 +0530

    minor refactoring

---
+++ Rocket.Chat/Managers/Model/SubscriptionManager/SubscriptionManager+Messages.swift
@@ -0,0 +1,52 @@
+//
+//  SubscriptionManager+Messages.swift
+//  Rocket.Chat
+//
+//  Created by Samar Sunkaria on 4/3/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
+
+extension SubscriptionManager {
+    static func markAsRead(_ subscription: Subscription, completion: @escaping MessageCompletion) {
+        let request = [
+            "msg": "method",
+            "method": "readMessages",
+            "params": [subscription.rid]
+            ] as [String: Any]
+
+        SocketManager.send(request) { response in
+            guard !response.isError() else { return Log.debug(response.result.string) }
+            completion(response)
+        }
+    }
+
+    static func sendTextMessage(_ message: Message, completion: @escaping MessageCompletion) {
+
+        let request = [
+            "msg": "method",
+            "method": "sendMessage",
+            "params": [[
+                "_id": message.identifier ?? "",
+                "rid": message.subscription.rid,
+                "msg": message.text
+                ]]
+            ] as [String: Any]
+
+        SocketManager.send(request) { (response) in
+            guard !response.isError() else { return Log.debug(response.result.string) }
+            completion(response)
+        }
+    }
+
+    static func toggleFavorite(_ subscription: Subscription, completion: @escaping MessageCompletion) {
+        let request = [
+            "msg": "method",
+            "method": "toggleFavorite",
+            "params": [subscription.rid, !subscription.favorite]
+            ] as [String: Any]
+
+        SocketManager.send(request, completion: completion)
+    }
+}
