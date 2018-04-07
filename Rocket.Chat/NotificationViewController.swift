7a66afde4e3c36a757b741e55f1106e4f4fa84aa
Samar Sunkaria <sameesunkaria@me.com>
2018-04-07 18:51:48 +0530

    Notification view positioned correctly

--- Rocket.Chat/NotificationViewController.swift
+++ Rocket.Chat/NotificationViewController.swift
@@ -36,60 +36,30 @@ class NotificationViewController: UIViewController {
         super.viewDidLoad()
         view.layer.shadowColor = UIColor.black.cgColor
         view.layer.shadowOpacity = 0.2
+        view.layer.shadowRadius = 8.0
         view.layer.shadowOffset = CGSize(width: 0, height: 0)
+        view.clipsToBounds = true
     }
 
     @IBOutlet private weak var hiddenConstraint: NSLayoutConstraint!
     @IBOutlet private weak var visibleConstraint: NSLayoutConstraint!
 
-//    func applyConstraints(to notificationView: NotificationView) {
-//        notificationView.translatesAutoresizingMaskIntoConstraints = false
-//
-//        NSLayoutConstraint.deactivate(constraints)
-//        hiddenConstraint?.isActive = false
-//        visibleConstraint?.isActive = false
-//        constraints.removeAll()
-//        switch traitCollection.horizontalSizeClass {
-//        case .compact: applyConstraintsForCompactWidth()
-//        case .regular: applyConstraintsForCompactWidth()
-//        default: break
-//        }
-////        let widthConstraint = notificationView.widthAnchor.constraint(equalTo: view.widthAnchor)
-////        widthConstraint.priority = UILayoutPriority.init(rawValue: 999)
-////        NSLayoutConstraint.activate([
-//////            notificationView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//////            notificationView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-////            notificationView.widthAnchor.constraint(lessThanOrEqualToConstant: 500),
-////            notificationView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-////            widthConstraint
-////        ])
-//        visibleConstraint = notificationView.topAnchor.constraint(equalTo: view.topAnchor)
-//        hiddenConstraint = notificationView.bottomAnchor.constraint(equalTo: view.topAnchor)
-//        hiddenConstraint?.isActive = true
-//    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
 
-//    func applyConstraintsForCompactWidth() {
-//        guard let notificationView = notificationView else { return }
-//
-//        constraints = [
-//            notificationView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//            notificationView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-//        ]
-//        NSLayoutConstraint.activate(constraints)
-//    }
+        visibleConstraint.constant = 8
+        if #available(iOS 11.0, *) {
+            if view.safeAreaInsets.top > 20 {
+                visibleConstraint.constant = 38
+            }
+        }
+    }
 
-//    override func loadView() {
-//        super.loadView()
-//        view = UIView()
-//        view.layer.masksToBounds = true
-//    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        let notificationView = Bundle.main.loadNibNamed("NotificationView", owner: self, options: nil)?.first as? NotificationView
-//        notificationView?.translatesAutoresizingMaskIntoConstraints = false
-//        self.notificationView = notificationView
-//    }
+    weak var timer: Timer? {
+        willSet {
+            timer?.invalidate()
+        }
+    }
 
     func displayNotification(title: String, body: String, user: User) {
         guard let notificationView = notificationView else { return }
@@ -100,11 +70,16 @@ class NotificationViewController: UIViewController {
             Animation(duration: 0.3, closure: {
                 self.notificationViewIsHidden = false
                 self.view.layoutIfNeeded()
-            }),
-            Animation(delay: 3, duration: 0.3, closure: {
-                self.notificationViewIsHidden = true
-                self.view.layoutIfNeeded()
             })
         ])
+
+        timer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false, block: { _ in
+            UIView.animate([
+                Animation(duration: 0.3, closure: {
+                    self.notificationViewIsHidden = true
+                    self.view.layoutIfNeeded()
+                })
+            ])
+        })
     }
 }
