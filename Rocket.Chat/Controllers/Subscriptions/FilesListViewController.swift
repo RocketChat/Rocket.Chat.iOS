e37a28de3e73680c3e6f870d07777e77d6266245
filipealva <ofilipealvarenga@gmail.com>
2018-04-12 00:26:43 -0300

    Draw and implement FilesListViewController

--- Rocket.Chat/Controllers/Subscriptions/FilesListViewController.swift
+++ Rocket.Chat/Controllers/Subscriptions/FilesListViewController.swift
@@ -8,28 +8,172 @@
 
 import UIKit
 
-class FilesListViewController: UIViewController {
+class FilesListViewData {
+    var subscription: Subscription?
 
+    let pageSize = 20
+    var currentPage = 0
+
+    var showing: Int = 0
+    var total: Int = 0
+
+    var title: String = localized("chat.messages.list.title") // TODO: Update
+
+    var isShowingAllFiles: Bool {
+        return showing >= total
+    }
+
+    var cells: [File] = []
+
+    func cell(at index: Int) -> File {
+        return cells[cells.index(cells.startIndex, offsetBy: index)]
+    }
+
+    private var isLoadingMoreFiles = false
+    func loadMoreFiles(completion: (() -> Void)? = nil) {
+        if isLoadingMoreFiles { return }
+
+        if let subscription = subscription {
+            isLoadingMoreFiles = true
+
+            let options = APIRequestOptions.paginated(count: pageSize, offset: currentPage*pageSize)
+            let filesRequest = SubscriptionFilesRequest(roomId: subscription.rid, subscriptionType: subscription.type)
+            API.current()?.fetch(filesRequest, options: options, succeeded: { result in
+                self.handle(result: result, completion: completion)
+            }, errored: { _ in
+                Alert.defaultError.present()
+            })
+        }
+    }
+
+    private func handle(result: SubscriptionFilesResult, completion: (() -> Void)? = nil) {
+        DispatchQueue.main.async {
+            self.showing += result.count ?? 0
+            self.total = result.total ?? 0
+
+            if let files = result.files {
+                guard !files.isEmpty else {
+                    self.isLoadingMoreFiles = false
+                    completion?()
+                    return
+                }
+
+                self.cells.append(contentsOf: files)
+            }
+
+            self.currentPage += 1
+
+            self.isLoadingMoreFiles = false
+            completion?()
+        }
+    }
+}
+
+class FilesListViewController: BaseViewController {
+    var data = FilesListViewData()
+
+    @IBOutlet weak var tableView: UITableView!
+
+    @objc func refreshControlDidPull(_ sender: UIRefreshControl) {
+        let data = FilesListViewData()
+        data.subscription = self.data.subscription
+        data.loadMoreFiles {
+            self.data = data
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.tableView.refreshControl?.endRefreshing()
+                self.updateIsEmptyFile()
+            }
+        }
+    }
+
+    func updateIsEmptyFile() {
+        guard let label = tableView.backgroundView as? UILabel else { return }
+
+        if data.cells.count == 0 {
+            label.text = localized("chat.messages.list.empty") // TODO: Update
+        } else {
+            label.text = ""
+        }
+    }
+
+    func loadMoreFiles() {
+        data.loadMoreFiles {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.tableView.refreshControl?.endRefreshing()
+                self.updateIsEmptyFile()
+            }
+        }
+    }
+}
+
+// MARK: ViewController
+
+extension FilesListViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlDidPull), for: .valueChanged)
+
+        tableView.refreshControl = refreshControl
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 70
+
+        let label = UILabel(frame: tableView.frame)
+        label.textAlignment = .center
+        label.textColor = .gray
+        tableView.backgroundView = label
+
+        registerCells()
+
+        title = data.title
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
-    /*
-    // MARK: - Navigation
+        loadMoreFiles()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        guard let refreshControl = tableView.refreshControl else { return }
+        tableView.refreshControl?.beginRefreshing()
+        tableView.contentOffset = CGPoint(x: 0, y: -refreshControl.frame.size.height)
     }
-    */
+
+    func registerCells() {
+        tableView.register(UINib(
+            nibName: String(describing: FileTableViewCell.self),
+            bundle: Bundle.main
+        ), forCellReuseIdentifier: FileTableViewCell.identifier)
+
+        tableView.register(UINib(
+            nibName: String(describing: LoaderTableViewCell.self),
+            bundle: Bundle.main
+        ), forCellReuseIdentifier: LoaderTableViewCell.identifier)
+    }
+}
+
+// MARK: UITableView
+
+extension FilesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.cells.count + (data.isShowingAllFiles ? 0 : 1)
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard indexPath.row < data.cells.count else {
+            loadMoreFiles()
+            return tableView.dequeueReusableCell(withIdentifier: LoaderTableViewCell.identifier, for: indexPath)
+        }
+
+        let file = data.cell(at: indexPath.row)
+        if let cell = tableView.dequeueReusableCell(withIdentifier: FileTableViewCell.identifier, for: indexPath) as? FileTableViewCell {
+            cell.file = file
+            return cell
+        }
+
+        return UITableViewCell()
+    }
 
 }
