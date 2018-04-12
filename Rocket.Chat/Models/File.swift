f39c38bbe21d103860a437a02a5312a6d45b1ac5
filipealva <ofilipealvarenga@gmail.com>
2018-04-12 00:27:06 -0300

    Implement File and FileModelMapping

--- Rocket.Chat/Models/File.swift
+++ Rocket.Chat/Models/File.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import RealmSwift
+
+class File: BaseModel {
+    @objc dynamic var name = ""
+    @objc dynamic var rid = ""
+    @objc dynamic var size: Double = 0
+    @objc dynamic var type = ""
+    @objc dynamic var fileDescription = ""
+    @objc dynamic var store = ""
+    @objc dynamic var isComplete = false
+    @objc dynamic var isUploading = false
+    @objc dynamic var fileExtension = ""
+    @objc dynamic var progress: Int = 0
+    @objc dynamic var user: User?
+    @objc dynamic var uploadedAt: Date?
+    @objc dynamic var url = ""
+}
+
+extension File {
+    var username: String {
+        return user?.username ?? ""
+    }
+}
--- Rocket.Chat/Models/Mapping/FileModelMapping.swift
+++ Rocket.Chat/Models/Mapping/FileModelMapping.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import SwiftyJSON
+import RealmSwift
+
+extension File: ModelMappeable {
+    func map(_ values: JSON, realm: Realm?) {
+        if self.identifier == nil {
+            self.identifier = values["_id"].stringValue
+        }
+
+        name = values["name"].stringValue
+        rid = values["rid"].stringValue
+        size = values["size"].double ?? 0
+        type = values["type"].stringValue
+        fileDescription = values["description"].stringValue
+        store = values["store"].stringValue
+        isComplete = values["complete"].bool ?? false
+        isUploading = values["uploading"].bool ?? false
+        fileExtension = values["extension"].stringValue
+        progress = values["progress"].int ?? 0
+        uploadedAt = Date.dateFromString(values["uploadedAt"].stringValue)
+        url = values["url"].stringValue
+
+        if let userIdentifier = values["userId"].string {
+            if let realm = realm {
+                if let user = realm.object(ofType: User.self, forPrimaryKey: userIdentifier as AnyObject) {
+                    self.user = user
+                }
+            }
+        }
+    }
+}
