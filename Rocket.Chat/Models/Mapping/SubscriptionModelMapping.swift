5f852b33bf0cf989c0bb846fbb9169546f782935
Rafael Kellermann Streit <rafaelks@me.com>
2018-05-15 20:46:03 -0300

    Do not make dumb updates on mapping & update subscription object mapping

--- Rocket.Chat/Models/Mapping/SubscriptionModelMapping.swift
+++ Rocket.Chat/Models/Mapping/SubscriptionModelMapping.swift
@@ -45,9 +45,18 @@ extension Subscription: ModelMappeable {
     func mapRoom(_ values: JSON) {
         self.roomDescription = values["description"].stringValue
         self.roomTopic = values["topic"].stringValue
-        self.roomReadOnly = values["ro"].boolValue
-        self.roomBroadcast = values["broadcast"].boolValue
-        self.roomOwnerId = values["u"]["_id"].stringValue
+
+        if let broadcast = values["broadcast"].bool {
+            self.roomBroadcast = broadcast
+        }
+
+        if let readOnly = values["ro"].bool {
+            self.roomReadOnly = readOnly
+        }
+
+        if let ownerId = values["u"]["_id"].string {
+            self.roomOwnerId = ownerId
+        }
 
         self.roomMuted.removeAll()
         if let roomMuted = values["muted"].array?.compactMap({ $0.string }) {
