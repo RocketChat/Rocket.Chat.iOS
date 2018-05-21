5316f04df611f8c43ddafeb96eb7cbda2b5a8699
cardoso <matheus@cardo.so>
2018-05-21 16:05:22 -0300

    Add DirectoryRequest

--- Rocket.Chat/Controllers/Chat/AddMembersViewController.swift
+++ Rocket.Chat/Controllers/Chat/AddMembersViewController.swift
@@ -6,9 +6,41 @@
 //  Copyright © 2018 Rocket.Chat. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
+class AddMembersViewData {
+    
+}
 
 class AddMembersViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: TableView
+
+extension AddMembersViewController: UITableViewDelegate {
+
+}
+
+extension AddMembersViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
