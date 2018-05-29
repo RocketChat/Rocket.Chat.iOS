7922d82d9d141e1e2852ffcbd5699016ed875675
filipealva <ofilipealvarenga@gmail.com>
2018-05-29 11:01:37 -0300

    Fix result sorting

--- Rocket.Chat/Extensions/Models/UserExtensions.swift
+++ Rocket.Chat/Extensions/Models/UserExtensions.swift
@@ -25,7 +25,10 @@ extension User {
         let usernamePredicate = NSPredicate(format: "username CONTAINS[c] %@", word)
         let nameOrUsernamePredicate = NSCompoundPredicate(orPredicateWithSubpredicates: [namePredicate, usernamePredicate])
         let users = (word.count > 0 ? realm.objects(User.self).filter(nameOrUsernamePredicate)
-            : realm.objects(User.self))
+            : realm.objects(User.self)).sorted(by: { user, _ -> Bool in
+                guard let username = user.username else { return false }
+                return preference.contains(username)
+            })
 
         let shouldUseRealName = AuthSettingsManager.settings?.useUserRealName ?? false
         (0..<min(limit, users.count)).forEach {
@@ -34,9 +37,6 @@ extension User {
             result.append((title, users[$0]))
         }
 
-        return result.sorted(by: { rhs, _ -> Bool in
-            guard let user = rhs.1 as? User, let username = user.username else { return false }
-            return preference.contains(username)
-        })
+        return result
     }
 }
