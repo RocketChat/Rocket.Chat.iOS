bee60c9c35b6ceb5fa2e9d2f3f08dfd8071b9e04
Samar Sunkaria <sameesunkaria@me.com>
2018-05-31 18:24:34 +0530

    Channel info action glyphs themed

--- Rocket.Chat/Views/Cells/Chat/Info/ChannelInfoActionCell.swift
+++ Rocket.Chat/Views/Cells/Chat/Info/ChannelInfoActionCell.swift
@@ -41,3 +41,11 @@ class ChannelInfoActionCell: UITableViewCell, ChannelInfoCellProtocol {
     @IBOutlet weak var imageViewIcon: UIImageView!
 
 }
+
+extension ChannelInfoActionCell {
+    override func applyTheme() {
+        super.applyTheme()
+        guard let theme = theme else { return }
+        imageViewIcon.tintColor = theme.titleText
+    }
+}
