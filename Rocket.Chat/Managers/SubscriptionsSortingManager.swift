d51f244f37370ce683f9fcfdeff080b97d44e5dc
Rafael Kellermann Streit <rafaelks@me.com>
2018-06-06 17:31:06 -0300

    Add documentation to the methods

--- Rocket.Chat/Managers/SubscriptionsSortingManager.swift
+++ Rocket.Chat/Managers/SubscriptionsSortingManager.swift
@@ -23,37 +23,62 @@ enum SubscriptionsGroupingOption: String {
 
 struct SubscriptionsSortingManager {
 
-    // This String is the prefix of each kind of sorting &
-    // grouping that will be persisted in UserDefaults.
+    /**
+     This String is the prefix of each kind of sorting &
+     grouping that will be persisted in UserDefaults.
+     */
     fileprivate static let kSortingKeyPrefix = "kSortingKey-"
 
-    // Returns the generated key to the setting
+    /**
+     - returns: The generated key to the setting
+     */
     fileprivate static func key(for option: String) -> String {
         return "\(kSortingKeyPrefix)\(option)"
     }
 
-    // Internal method to check the value of the key
-    // on user defaults
+    /**
+     Internal method to check the value of the key on user defaults
+
+     - parameters:
+        - option: The option to check if it is selected.
+     - returns: Returns if the option is selected or not.
+     */
     fileprivate static func isSelected(option: String) -> Bool {
         let keyName = key(for: option)
         return UserDefaults.group.bool(forKey: keyName)
     }
 
-    // This method updates the value of some key
+    /**
+     This method updates the value of some key
+
+     - parameters:
+        - option: The option to be updated.
+        - value: The new value of the option.
+     */
     fileprivate static func update(option: String, value: Bool) {
         let keyName = key(for: option)
         UserDefaults.group.set(value, forKey: keyName)
     }
 
-    // This method toggles the value of the option. If the option
-    // doesn't exist, the default value is false
+    /**
+     This method toggles the value of the option. If the option
+     doesn't exist, the default value is false
+
+     - parameters:
+        - option: The option to be toggled.
+     */
     static func toggle(option: String) {
         let keyName = key(for: option)
         let value = UserDefaults.group.bool(forKey: keyName)
         UserDefaults.group.set(!value, forKey: keyName)
     }
 
-    // Changes the sort option, because sort only allows one option to be enabled
+    /**
+     Changes the sort option, because sort only allows one option to be enabled.
+
+     - parameters:
+        - option: The sorting option to be selected.
+     */
     static internal func select(option: SubscriptionsSortingOption) {
         update(option: option.rawValue, value: true)
 
@@ -64,7 +89,9 @@ struct SubscriptionsSortingManager {
         }
     }
 
-    // Returns the selected sorting option
+    /**
+     The selected sorting option
+     */
     static internal var selectedSortingOption: SubscriptionsSortingOption {
         if isSelected(option: SubscriptionsSortingOption.alphabetically.rawValue) {
             return .alphabetically
@@ -73,7 +100,9 @@ struct SubscriptionsSortingManager {
         return .activity
     }
 
-    // Returns the selected grouping options
+    /**
+     The selected grouping options
+     */
     static internal var selectedGroupingOptions: [SubscriptionsGroupingOption] {
         var options: [SubscriptionsGroupingOption] = []
 
