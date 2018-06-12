d074fd5b138a869fb2d48e9b30e04004be8b73d5
filipealva <ofilipealvarenga@gmail.com>
2018-06-12 02:23:20 -0300

    Draw SignupTableViewController

---
+++ Rocket.Chat/Controllers/Auth/SignupTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignupTableViewController.swift
+//  Rocket.Chat
+//
+//  Created by Filipe Alvarenga on 12/06/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+
+class SignupTableViewController: UITableViewController {
+
+    // MARK: Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(tapGesture)
+    }
+
+    // MARK: Keyboard Management
+
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+
+
+}
