86f84b9fca8281e9ee5d2fa7088f35453744cb33
filipealva <ofilipealvarenga@gmail.com>
2018-06-12 22:20:24 -0300

    Adapt AuthTableViewControllerLoginServices to work based on login services list

--- Rocket.Chat/Controllers/Auth/AuthTableViewControllerLoginServices.swift
+++ Rocket.Chat/Controllers/Auth/AuthTableViewControllerLoginServices.swift
@@ -12,38 +12,6 @@ import RealmSwift
 
 extension AuthTableViewController {
 
-    func updateAuthenticationMethods() {
-        guard let settings = serverPublicSettings else { return }
-
-        if settings.isGoogleAuthenticationEnabled {
-            addOAuthButton(for: .google)
-        }
-
-        if settings.isFacebookAuthenticationEnabled {
-            addOAuthButton(for: .facebook)
-        }
-
-        if settings.isGitHubAuthenticationEnabled {
-            addOAuthButton(for: .github)
-        }
-
-        if settings.isGitLabAuthenticationEnabled {
-            addOAuthButton(for: .gitlab(url: settings.gitlabUrl))
-        }
-
-        if settings.isLinkedInAuthenticationEnabled {
-            addOAuthButton(for: .linkedin)
-        }
-
-        if settings.isWordPressAuthenticationEnabled {
-            addOAuthButton(for: .wordpress)
-        }
-
-        if settings.isCASEnabled {
-            addOAuthButton(for: .cas)
-        }
-    }
-
     func setupLoginServices() {
         guard shouldRetrieveLoginServices else {
             return
@@ -65,13 +33,13 @@ extension AuthTableViewController {
             strongSelf.startLoading()
 
             AuthManager.auth(credentials: credentials, completion: strongSelf.handleAuthenticationResponse)
-            }, failure: { [weak self] in
-                self?.alert(
-                    title: localized("alert.login_service_error.title"),
-                    message: localized("alert.login_service_error.message")
-                )
+        }, failure: { [weak self] in
+            self?.alert(
+                title: localized("alert.login_service_error.title"),
+                message: localized("alert.login_service_error.message")
+            )
 
-                self?.stopLoading()
+            self?.stopLoading()
         })
     }
 
@@ -116,71 +84,27 @@ extension AuthTableViewController {
         return
     }
 
-    func addOAuthButton(for loginService: LoginService) {
-        guard let service = loginService.service else { return }
-
-//        let button = customAuthButtons[service] ?? UIButton()
-        let button = UIButton() // TODO: Fix it
-
-        switch loginService.type {
-        case .google: button.setImage(#imageLiteral(resourceName: "google"), for: .normal)
-        case .facebook: button.setImage(#imageLiteral(resourceName: "facebook"), for: .normal)
-        case .github: button.setImage(#imageLiteral(resourceName: "github"), for: .normal)
-        case .gitlab: button.setImage(#imageLiteral(resourceName: "gitlab"), for: .normal)
-        case .linkedin: button.setImage(#imageLiteral(resourceName: "linkedin"), for: .normal)
-        case .wordpress: button.setImage(#imageLiteral(resourceName: "wordpress"), for: .normal)
-        default: button.setTitle(loginService.buttonLabelText ?? "", for: .normal)
-        }
-
-        button.layer.cornerRadius = 3
-        button.titleLabel?.font = .boldSystemFont(ofSize: 17.0)
-        button.titleLabel?.adjustsFontSizeToFitWidth = true
-        button.setTitleColor(UIColor(hex: loginService.buttonLabelColor), for: .normal)
-        button.backgroundColor = UIColor(hex: loginService.buttonColor)
-
-//        if !authButtonsStackView.subviews.contains(button) {
-//            authButtonsStackView.addArrangedSubview(button)
-//            button.addTarget(self, action: #selector(loginServiceButtonDidPress(_:)), for: .touchUpInside)
-//            customAuthButtons[service] = button
-//        }
-    }
-
     func updateLoginServices(changes: RealmCollectionChange<Results<LoginService>>) {
         switch changes {
         case .update(let res, let deletions, let insertions, let modifications):
-            let loginServices = insertions.map { res[$0] }.compactMap {
+            self.loginServices.append(contentsOf: insertions.map { res[$0] }.compactMap {
                 return ($0.isValid && $0.service != nil) ? $0 : nil
-            }
-
-//            self.loginServices = loginServices
-            self.loginServices.append(contentsOf: loginServices)
+            })
 
             modifications.map { res[$0] }.forEach {
-                guard
-                    let identifier = $0.identifier
-//                    let button = customAuthButtons[identifier]
-                    // TODO: Fixit
-                    else {
-                        return
+                guard let index = self.loginServices.index(of: $0) else {
+                    return
                 }
 
-                let button = UIButton() // TODO: Fixit
-                button.setTitle($0.buttonLabelText ?? "", for: .normal)
-                button.setTitleColor(UIColor(hex: $0.buttonLabelColor), for: .normal)
-                button.backgroundColor = UIColor(hex: $0.buttonColor)
+                self.loginServices[index] = $0
             }
 
             deletions.map { res[$0] }.forEach {
-                guard
-                    $0.custom,
-                    let identifier = $0.identifier
-//                    let button = customAuthButtons[identifier]
-                    else {
-                        return
+                guard let index = self.loginServices.index(of: $0) else {
+                    return
                 }
 
-//                authButtonsStackView.removeArrangedSubview(button)
-//                customAuthButtons.removeValue(forKey: identifier)
+                self.loginServices.remove(at: index)
             }
         default: break
         }
