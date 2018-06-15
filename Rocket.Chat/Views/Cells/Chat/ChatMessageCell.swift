dea45605174a8ef06e7e78392efe45975266cf82
Rafael Kellermann Streit <rafaelks@me.com>
2018-06-15 18:10:22 -0300

    Spacing adjustments

--- Rocket.Chat/Views/Cells/Chat/ChatMessageCell.swift
+++ Rocket.Chat/Views/Cells/Chat/ChatMessageCell.swift
@@ -322,9 +322,9 @@ extension ChatMessageCell {
         let fullWidth = width
         let attributedString = MessageTextCacheManager.shared.message(for: message, with: nil)
 
-        var total = (CGFloat)(sequential ? 8 : 29) + (message.reactions.count > 0 ? 40 : 0)
+        var total = (CGFloat)(sequential ? 8 : 35) + (message.reactions.count > 0 ? 40 : 0)
         if attributedString?.string ?? "" != "" {
-            total += (attributedString?.heightForView(withWidth: fullWidth - 55) ?? 0)
+            total += (attributedString?.heightForView(withWidth: fullWidth - 65) ?? 0)
         }
 
         if message.isBroadcastReplyAvailable() {
