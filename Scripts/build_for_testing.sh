b86976a02095fc4313cebf08fb3c6ed56d969ebc
Samar Sunkaria <sameesunkaria@me.com>
2018-07-02 22:21:39 +0530

    Updated to build the project under a Release configuration for testing

--- Scripts/build_for_testing.sh
+++ Scripts/build_for_testing.sh
@@ -3,4 +3,5 @@
 xcodebuild clean build-for-testing \
     -workspace Rocket.Chat.xcworkspace \
     -sdk iphonesimulator \
+    -configuration Release
     -scheme Rocket.Chat | xcpretty -c && exit ${PIPESTATUS[0]}
