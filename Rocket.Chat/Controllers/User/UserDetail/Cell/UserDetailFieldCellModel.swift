87b8fc807fc11c8901e8e91d5e29e27a19266944
cardoso <matheus@cardo.so>
2018-07-03 09:05:36 -0300

    Localizer User Detail strings

--- Rocket.Chat/Controllers/User/UserDetail/Cell/UserDetailFieldCellModel.swift
+++ Rocket.Chat/Controllers/User/UserDetail/Cell/UserDetailFieldCellModel.swift
@@ -28,23 +28,23 @@ extension UserDetailFieldCellModel {
         var cells = [UserDetailFieldCellModel]()
         let createCell = UserDetailFieldCellModel.init
 
-        cells.append(createCell("Status", "\(user.status)"))
+        cells.append(createCell(localized("user_details.status"), "\(user.status)"))
 
         if let role = user.roles.first {
             let roles = user.roles.dropFirst().reduce("\(role)") { "\($0), \($1)" }
-            cells.append(createCell(roles.count > 1 ? "Roles" : "Role", roles))
+            cells.append(createCell(roles.count > 1 ? localized("user_details.roles") : localized("user_details.role"), roles))
         }
 
         let emails = user.emails.map { $0.email }
         if let email = emails.first {
             let emails = emails.dropFirst().reduce("\(email)") { "\($0), \($1)" }
-            cells.append(createCell(emails.count > 1 ? "Emails" : "Email", emails))
+            cells.append(createCell(emails.count > 1 ? localized("user_details.emails") : localized("user_details.email"), emails))
         }
 
         let sign = user.utcOffset < 0 ? "" : "+"
         let offset = user.utcOffset.truncatingRemainder(dividingBy: 1) == 0 ? "\(Int(user.utcOffset))" : "\(user.utcOffset)"
         let timeZone = TimeZone(secondsFromGMT: Int(user.utcOffset * 60 * 60))
-        cells.append(createCell("Timezone", "(UTC \(sign)\(offset)) \(Date().formatted("hh:mm a", timeZone: timeZone))"))
+        cells.append(createCell(localized("user_details.timezone"), "(UTC \(sign)\(offset)) \(Date().formatted("hh:mm a", timeZone: timeZone))"))
 
         return cells
     }
