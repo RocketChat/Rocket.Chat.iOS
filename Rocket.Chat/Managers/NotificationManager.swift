512c9c3f7ed1b94b208336837c05e9cfbb32229b
Samar Sunkaria <sameesunkaria@me.com>
2018-07-11 13:27:36 +0530

    Fix crash due to in-app notifications

--- Rocket.Chat/Managers/NotificationManager.swift
+++ Rocket.Chat/Managers/NotificationManager.swift
@@ -15,36 +15,40 @@ final class NotificationManager {
 
     /// Posts an in-app notification.
     ///
+    /// This method is thread safe, and therefore can be called from any thread.
+    ///
     /// **NOTE:** The notification is only posted if the `rid` of the
     /// notification is different from the `AppManager.currentRoomId`
     ///
     /// - parameters:
-    ///     - notification: The `ChatNotification` object to display the
-    ///         contents of the notification from. The `title` and the `body`
-    ///         cannot be empty strings.
+    ///     - notification: The `ChatNotification` object used to display the
+    ///         contents of the notification. The `title` and the `body` of the
+    ///         notification cannot be empty strings.
 
     static func post(notification: ChatNotification) {
-        guard
-            AppManager.currentRoomId != notification.payload.rid,
-            !notification.title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty,
-            !notification.body.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
-        else {
-            return
+        DispatchQueue.main.async {
+            guard
+                AppManager.currentRoomId != notification.payload.rid,
+                !notification.title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty,
+                !notification.body.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+            else {
+                return
+            }
+
+            let formattedBody = NSMutableAttributedString(string: notification.body)
+                .transformMarkdown().string
+                .components(separatedBy: .newlines)
+                .joined(separator: " ")
+                .replacingOccurrences(of: "^\\s+", with: "", options: .regularExpression)
+
+            NotificationViewController.shared.displayNotification(
+                title: notification.title,
+                body: formattedBody,
+                username: notification.payload.sender.username
+            )
+
+            NotificationManager.shared.notification = notification
         }
-
-        let formattedBody = NSMutableAttributedString(string: notification.body)
-            .transformMarkdown().string
-            .components(separatedBy: .newlines)
-            .joined(separator: " ")
-            .replacingOccurrences(of: "^\\s+", with: "", options: .regularExpression)
-
-        NotificationViewController.shared.displayNotification(
-            title: notification.title,
-            body: formattedBody,
-            username: notification.payload.sender.username
-        )
-
-        NotificationManager.shared.notification = notification
     }
 
     func didRespondToNotification() {
