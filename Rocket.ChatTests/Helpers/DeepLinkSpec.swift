201aa848c5737508a4c5889e9f3a9682e017363d
filipealva <ofilipealvarenga@gmail.com>
2018-07-24 20:06:35 -0300

    Add DeepLinkSpec to test all possible URL to DeepLink parsing

--- Rocket.ChatTests/Helpers/DeepLinkSpec.swift
+++ Rocket.ChatTests/Helpers/DeepLinkSpec.swift
@@ -8,28 +8,24 @@
 
 import XCTest
 
+@testable import Rocket_Chat
+
 class DeepLinkSpec: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let validDeeplinkURLs = [
+        "https://go.rocket.chat/auth?host=open.rocket.chat&token=lkjadfalkdj2fe0f23f802f23080283hf08hsd0f08h&userId=initialuser",
+        "https://go.rocket.chat/auth?host=open.rocket.chat",
+        "https://go.rocket.chat/room?host=open.rocket.chat&rid=asdfasdf",
+        "https://go.rocket.chat/mention?name=test",
+        "https://go.rocket.chat/channel?name=general",
+        "rocketchat://auth?host=open.rocket.chat&token=lkjadfalkdj2fe0f23f802f23080283hf08hsd0f08h&userId=initialuser",
+        "rocketchat://auth?host=open.rocket.chat",
+        "rocketchat://room?host=open.rocket.chat&rid=asdfasdf",
+        "rocketchat://mention?name=test",
+        "rocketchat://channel?name=general"
+    ]
+
+    func testSupportedDeepLinks() {
+        let deeplinks = validDeeplinkURLs.compactMap({ URL(string: $0) }).compactMap { DeepLink(url: $0) }
+        XCTAssertEqual(deeplinks.count, validDeeplinkURLs.count, "Failed to parse one or more supported deeplink URLs")
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
