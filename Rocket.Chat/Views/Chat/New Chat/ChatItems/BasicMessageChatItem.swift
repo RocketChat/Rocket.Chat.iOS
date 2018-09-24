4d137db1fa2a1607a88acbc82bf8744bf0b3b9a4
filipealva <ofilipealvarenga@gmail.com>
2018-09-24 02:05:21 -0300

    Implement BasicMessageChatItem

--- Rocket.Chat/Views/Chat/New Chat/ChatItems/BasicMessageChatItem.swift
+++ Rocket.Chat/Views/Chat/New Chat/ChatItems/BasicMessageChatItem.swift
@@ -7,7 +7,28 @@
 //
 
 import Foundation
+import DifferenceKit
+import RocketChatViewController
 
-struct BasicMessageChatItem {
+struct BasicMessageChatItem: ChatItem, Differentiable {
+    var relatedReuseIdentifier: String {
+        return BasicMessageCell.identifier
+    }
 
+    var user: UnmanagedUser
+    var message: UnmanagedMessage
+
+    var differenceIdentifier: String {
+        return user.differenceIdentifier + message.identifier
+    }
+
+    func isContentEqual(to source: BasicMessageChatItem) -> Bool {
+        return
+            user.name == source.user.name &&
+            user.username == source.user.username &&
+            message.temporary == source.message.temporary &&
+            message.failed == source.message.failed &&
+            message.text == source.message.text &&
+            message.updatedAt?.timeIntervalSince1970 == source.message.updatedAt?.timeIntervalSince1970
+    }
 }
