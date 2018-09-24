4567b42de7abace45168422eea5d4c5775033763
filipealva <ofilipealvarenga@gmail.com>
2018-09-24 02:03:39 -0300

    Add BasicMessageCell and BasicMessageChatItem

---
+++ Rocket.Chat/Views/Chat/New Chat/Cells/BasicMessageCell.swift
@@ -0,0 +1,13 @@
+//
+//  BasicMessageCell.swift
+//  Rocket.Chat
+//
+//  Created by Filipe Alvarenga on 23/09/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+
+class BasicMessageCell: UICollectionViewCell {
+    
+}
