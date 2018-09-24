76356face0addf91d0ec7b36254f5ef112aebfb6
filipealva <ofilipealvarenga@gmail.com>
2018-09-24 02:05:45 -0300

    Add basic implementation of MessageSection

--- Rocket.Chat/Controllers/Chat/ChatSections/MessageSection.swift
+++ Rocket.Chat/Controllers/Chat/ChatSections/MessageSection.swift
@@ -7,7 +7,33 @@
 //
 
 import Foundation
+import DifferenceKit
+import RocketChatViewController
 
-struct MessageSection {
+struct MessageSection: ChatSection {
+    var object: AnyDifferentiable
 
+    func viewModels() -> [AnyChatItem] {
+        guard
+            let object = object.base as? MessageSectionModel,
+            let user = object.message.user
+        else {
+            return []
+        }
+
+        let basicMessageItem = BasicMessageChatItem(
+            user: user,
+            message: object.message
+        ).wrapped
+
+        return [basicMessageItem]
+    }
+
+    func cell(for viewModel: AnyChatItem, on collectionView: UICollectionView, at indexPath: IndexPath) -> ChatCell {
+        var cell = collectionView.dequeueChatCell(withReuseIdentifier: viewModel.relatedReuseIdentifier, for: indexPath)
+        cell.viewModel = viewModel
+        cell.configure()
+
+        return cell
+    }
 }
