d6585ea318e6ca8d713ed135c5eece56dee1c07c
filipealva <ofilipealvarenga@gmail.com>
2018-09-24 02:07:12 -0300

    Add basic implementation for MessagesViewController to support basic messages (using local database messages until we don't have our new DataController)

--- Rocket.Chat/Controllers/Chat/MessagesViewController.swift
+++ Rocket.Chat/Controllers/Chat/MessagesViewController.swift
@@ -8,12 +8,41 @@
 
 import UIKit
 import RocketChatViewController
+import RealmSwift
+import DifferenceKit
 
 final class MessagesViewController: RocketChatViewController {
     var subscription: Subscription!
+    var heightCache: [AnyHashable: CGFloat] = [:]
+    var sectionsToAddLater: [AnyChatSection] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        collectionView.register(BasicMessageCell.nib, forCellWithReuseIdentifier: BasicMessageCell.identifier)
+
+        var messageSections = Array(subscription.messages.map(Message.init).map { (message) -> AnyChatSection in
+            let messageSectionModel = MessageSectionModel(message: message.unmanaged, daySeparator: nil, isLoadingMore: false, isNew: false)
+            let messageSection = MessageSection(object: AnyDifferentiable(messageSectionModel))
+            return AnyChatSection(messageSection)
+        })
+
+        if messageSections.count > 30 {
+            for index in 1..<20 {
+                sectionsToAddLater.append(messageSections[index])
+                messageSections.remove(at: index)
+            }
+        }
+
+        data = messageSections
+        updateData()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        data.append(contentsOf: sectionsToAddLater)
+        updateData()
     }
 
     func openURL(url: URL) {
