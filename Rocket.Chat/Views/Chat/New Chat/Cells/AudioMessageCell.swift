fb1865bb63f223cbb4aa64375d9c783c84fb1aeb
Rafael Kellermann Streit <rafaelks@me.com>
2018-09-28 07:55:19 -0300

    Create the initial cell view and cell model for Audio player

---
+++ Rocket.Chat/Views/Chat/New Chat/Cells/AudioMessageCell.swift
@@ -0,0 +1,47 @@
+//
+//  AudioMessageCell.swift
+//  Rocket.Chat
+//
+//  Created by Rafael Streit on 28/09/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
+import RocketChatViewController
+
+final class AudioMessageCell: UICollectionViewCell, ChatCell, SizingCell {
+    static let identifier = String(describing: AudioMessageCell.self)
+
+    static let sizingCell: UICollectionViewCell & ChatCell = {
+        guard let cell = AudioMessageCell.instantiateFromNib() else {
+            return AudioMessageCell()
+        }
+
+        return cell
+    }()
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+    var viewModel: AnyChatItem?
+    var contentViewWidthConstraint: NSLayoutConstraint!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentViewWidthConstraint = contentView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width)
+        contentViewWidthConstraint.isActive = true
+    }
+
+    func configure() {
+        updateText()
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        titleLabel.text = nil
+        descriptionLabel.text = nil
+    }
+
+}
---
+++ Rocket.Chat/Views/Chat/New Chat/ChatItems/AudioMessageChatItem.swift
@@ -0,0 +1,32 @@
+//
+//  AudioMessageChatItem.swift
+//  Rocket.Chat
+//
+//  Created by Rafael Streit on 28/09/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
+import DifferenceKit
+import RocketChatViewController
+
+struct AudioMessageChatItem: ChatItem, Differentiable {
+    var relatedReuseIdentifier: String {
+        return SequentialMessageCell.identifier
+    }
+
+    var title: String?
+    var description: String?
+    var audioURL: URL?
+
+    var differenceIdentifier: String {
+        return audioURL?.absoluteString ?? ""
+    }
+
+    func isContentEqual(to source: AudioMessageChatItem) -> Bool {
+        return
+            title == source.title &&
+            description == source.description &&
+            audioURL == source.audioURL
+    }
+}
