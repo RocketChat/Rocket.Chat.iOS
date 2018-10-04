49ca68e4feefb77cd810f948c3eb1bb58b5884a2
filipealva <ofilipealvarenga@gmail.com>
2018-10-03 21:42:15 -0300

    Implement QuoteCell and QuoteChatItem

--- Rocket.Chat/Views/Chat/New Chat/ChatItems/QuoteChatItem.swift
+++ Rocket.Chat/Views/Chat/New Chat/ChatItems/QuoteChatItem.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import DifferenceKit
+import RocketChatViewController
+import RealmSwift
+
+struct QuoteChatItem: ChatItem, Differentiable {
+    var relatedReuseIdentifier: String {
+        return QuoteCell.identifier
+    }
+
+    var attachment: Attachment
+
+    var differenceIdentifier: String {
+        return attachment.identifier ?? ""
+    }
+
+    func isContentEqual(to source: QuoteChatItem) -> Bool {
+        return attachment.title == source.attachment.title &&
+            attachment.text == source.attachment.text &&
+            attachment.collapsed == source.attachment.collapsed
+    }
+
+    func toggle() {
+        Realm.executeOnMainThread({ _ in
+            self.attachment.collapsed = !self.attachment.collapsed
+        })
+    }
+}
