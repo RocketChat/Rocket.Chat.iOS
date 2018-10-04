19e3c102d121e177aefaacb9b2fcfb194d0eec18
filipealva <ofilipealvarenga@gmail.com>
2018-10-03 21:41:02 -0300

    Add quote cell and quote cell chat item

---
+++ Rocket.Chat/Views/Chat/New Chat/Cells/QuoteCell.swift
@@ -0,0 +1,13 @@
+//
+//  QuoteCell.swift
+//  Rocket.Chat
+//
+//  Created by Filipe Alvarenga on 03/10/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+
+class QuoteCell: UICollectionViewCell {
+    
+}
---
+++ Rocket.Chat/Views/Chat/New Chat/ChatItems/QuoteChatItem.swift
@@ -0,0 +1,9 @@
+//
+//  QuoteChatItem.swift
+//  Rocket.Chat
+//
+//  Created by Filipe Alvarenga on 03/10/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
