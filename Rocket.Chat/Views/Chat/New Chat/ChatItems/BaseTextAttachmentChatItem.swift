30364664cd581cc7b660ab307d79a6cde3169345
filipealva <ofilipealvarenga@gmail.com>
2018-11-06 06:08:54 -0200

    Add BaseTextAttachment to avoid repeating collapsed property

---
+++ Rocket.Chat/Views/Chat/New Chat/ChatItems/BaseTextAttachmentChatItem.swift
@@ -0,0 +1,9 @@
+//
+//  BaseTextAttachmentChatItem.swift
+//  Rocket.Chat
+//
+//  Created by Filipe Alvarenga on 06/11/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
