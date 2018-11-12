c13fc28de754f565f4dee1821c77882dbeba9688
cardoso <matheus@cardo.so>
2018-11-12 15:05:12 -0300

    Move some extensions to other files

---
+++ Rocket.Chat/Controllers/Chat/MessageViewControllerEmptyState.swift
@@ -0,0 +1,38 @@
+//
+//  MessageViewControllerEmptyState.swift
+//  Rocket.Chat
+//
+//  Created by Matheus Cardoso on 11/12/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+extension MessagesViewController {
+    func updateEmptyState() {
+        if subscription == nil {
+            title = ""
+            composerView.isHidden = true
+
+            chatTitleView?.removeFromSuperview()
+            emptyStateImageView?.removeFromSuperview()
+
+            guard let theme = view.theme else { return }
+            let themeName = ThemeManager.themes.first { $0.theme == theme }?.title
+
+            let imageView = UIImageView(image: UIImage(named: "Empty State \(themeName ?? "light")"))
+            imageView.contentMode = .scaleAspectFill
+            imageView.clipsToBounds = true
+            view.addSubview(imageView)
+
+            emptyStateImageView = imageView
+
+            updateEmptyStateFrame()
+        } else {
+            emptyStateImageView?.removeFromSuperview()
+        }
+    }
+
+    func updateEmptyStateFrame() {
+        emptyStateImageView?.frame = view.bounds
+    }
+}
+
