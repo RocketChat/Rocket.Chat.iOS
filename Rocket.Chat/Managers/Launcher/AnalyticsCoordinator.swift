8bacebd5f3077679ebd2930301f1a79a7dbb8df6
filipealva <ofilipealvarenga@gmail.com>
2018-11-12 11:51:27 -0200

    Remove identify/anonymize mechanisms from AnalyticsCoordinator

--- Rocket.Chat/Managers/Launcher/AnalyticsCoordinator.swift
+++ Rocket.Chat/Managers/Launcher/AnalyticsCoordinator.swift
@@ -57,45 +57,5 @@ struct AnalyticsCoordinator: LauncherProtocol {
 
     private func launchFabric() {
         Fabric.with([Crashlytics.self])
-
-        if let currentUser = AuthManager.currentUser() {
-            AnalyticsCoordinator.identifyCrashReports(withUser: currentUser)
-        } else {
-            AnalyticsCoordinator.anonymizeCrashReports()
-        }
-    }
-
-    static func identifyCrashReports(withUser user: User) {
-        guard let id = user.identifier else {
-            return
-        }
-
-        let crashlytics = Crashlytics.sharedInstance()
-        crashlytics.setUserIdentifier(id)
-
-        if let name = user.name {
-            crashlytics.setUserName(name)
-        }
-
-        if let email = user.emails.first?.email {
-            crashlytics.setUserEmail(email)
-        }
-
-        if let serverURL = AuthManager.selectedServerInformation()?[ServerPersistKeys.serverURL] {
-            crashlytics.setObjectValue(serverURL, forKey: ServerPersistKeys.serverURL)
-        }
-
-        if let serverVersion = AuthManager.selectedServerInformation()?[ServerPersistKeys.serverVersion] {
-            crashlytics.setObjectValue(serverVersion, forKey: ServerPersistKeys.serverVersion)
-        }
-    }
-
-    static func anonymizeCrashReports() {
-        let crashlytics = Crashlytics.sharedInstance()
-
-        crashlytics.setUserEmail(nil)
-        crashlytics.setUserName(nil)
-        crashlytics.setUserIdentifier(nil)
-        crashlytics.setObjectValue(nil, forKey: ServerPersistKeys.serverURL)
     }
 }
