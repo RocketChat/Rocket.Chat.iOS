d30947409185135555966c1cba4496538c1010ac
Rafael Kellermann Streit <rafaelks@me.com>
2018-11-22 21:57:26 -0200

    Use images instead of tint the image

--- Rocket.Chat/Views/Chat/New Chat/Cells/BaseMessageCell.swift
+++ Rocket.Chat/Views/Chat/New Chat/Cells/BaseMessageCell.swift
@@ -64,7 +64,7 @@ class BaseMessageCell: UICollectionViewCell, ChatCell {
             button.changeWidth(to: 0)
             button.changeLeading(to: 0)
 
-            let image = UIImage(named: "Read")?.imageWithTint(viewModel.isUnread ? #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1) : #colorLiteral(red: 0.1137254902, green: 0.4549019608, blue: 0.9607843137, alpha: 1), alpha: 0.0)
+            let image = viewModel.isUnread ? UIImage(named: "Unread") : UIImage(named: "Read")
             button.setImage(image, for: .normal)
         }
     }
