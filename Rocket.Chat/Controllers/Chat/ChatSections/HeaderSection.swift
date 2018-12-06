f08e5fa045fee9e2b99da38d90a9fa592813f507
filipealva <ofilipealvarenga@gmail.com>
2018-12-06 17:48:12 -0200

    Implement ChatSection protocol for HeaderSection

--- Rocket.Chat/Controllers/Chat/ChatSections/HeaderSection.swift
+++ Rocket.Chat/Controllers/Chat/ChatSections/HeaderSection.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import DifferenceKit
+import RocketChatViewController
+
+struct HeaderSection: ChatSection {
+    var object: AnyDifferentiable
+    var controllerContext: UIViewController?
+    var messagesController: MessagesViewController? {
+        return controllerContext as? MessagesViewController
+    }
+
+    func viewModels() -> [AnyChatItem] {
+        guard let headerChatItem = object.base as? HeaderChatItem else {
+            return []
+        }
+
+        return [headerChatItem.wrapped]
+    }
+
+    func cell(for viewModel: AnyChatItem, on collectionView: UICollectionView, at indexPath: IndexPath) -> ChatCell {
+        var cell = collectionView.dequeueChatCell(withReuseIdentifier: viewModel.relatedReuseIdentifier, for: indexPath)
+
+        cell.messageWidth = messagesController?.messageWidth() ?? 0
+        cell.viewModel = viewModel
+        cell.configure(completeRendering: true)
+        return cell
+    }
+}
