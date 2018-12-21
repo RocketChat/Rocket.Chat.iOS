5eb80e2ed95d0b9bf30cebf1a3361ea3d8197c08
Matheus Cardoso <matheus@cardo.so>
2018-12-21 12:05:31 -0300

    WIP: Fix not sending typing event

--- Rocket.Chat/Controllers/Chat/MessagesViewController.swift
+++ Rocket.Chat/Controllers/Chat/MessagesViewController.swift
@@ -188,6 +188,7 @@ final class MessagesViewController: RocketChatViewController {
             return self.viewModel.recentSenders
         }
 
+        startTypingHandler()
         startDraftMessage()
         updateJoinedView()
     }
