f0a9a1b81995e7e6cb13dc5aabdc816ebe139bdd
Matheus Cardoso <matheus@cardo.so>
2018-12-29 18:31:43 -0300

    Fix long lines

---
+++ Rocket.ChatTests/API/Requests/Room/RoomHistoryRequestSpec.swift
@@ -0,0 +1,9 @@
+//
+//  RoomHistoryRequestSpec.swift
+//  Rocket.ChatTests
+//
+//  Created by Matheus Cardoso on 29/12/2018.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
