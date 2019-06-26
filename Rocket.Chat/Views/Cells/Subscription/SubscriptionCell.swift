91d3dade67117a83229f6670374d23a06ac77d1e
Rudrank <rudrankriyam@gmail.com>
2019-06-27 02:21:16 +0530

    Add message: in front of the message

--- Rocket.Chat/Views/Cells/Subscription/SubscriptionCell.swift
+++ Rocket.Chat/Views/Cells/Subscription/SubscriptionCell.swift
@@ -27,6 +27,7 @@ final class SubscriptionCell: BaseSubscriptionCell {
         guard let subscription = subscription?.managedObject else { return }
 
         labelLastMessage.text = subscription.roomLastMessageText ?? localized("subscriptions.list.no_message")
+        labelLastMessage.accessibilityLabel = [VOLocalizedString("subscriptions.list.message.label"), labelLastMessage.text].compactMap {$0}.joined()
 
         if let roomLastMessage = subscription.roomLastMessage?.createdAt {
             labelDate.text = dateFormatted(date: roomLastMessage)
