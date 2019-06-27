8883bc7b86121ee04374cb91e64ce311d74e159e
Rudrank <rudrankriyam@gmail.com>
2019-06-28 01:43:05 +0530

    Notification for Messages and Servers heading

--- Rocket.Chat/Controllers/Subscriptions/SubscriptionsList/SubscriptionsViewController.swift
+++ Rocket.Chat/Controllers/Subscriptions/SubscriptionsList/SubscriptionsViewController.swift
@@ -138,6 +138,7 @@ final class SubscriptionsViewController: BaseViewController {
         if !(searchBar?.text?.isEmpty ?? true) {
             searchBar?.perform(#selector(becomeFirstResponder), with: nil, afterDelay: 0.1)
         }
+        UIAccessibility.post(notification: UIAccessibility.Notification.screenChanged, argument: titleView?.labelMessages)
     }
 
     override func viewDidDisappear(_ animated: Bool) {
@@ -360,6 +361,7 @@ extension SubscriptionsViewController: UISearchBarDelegate {
 
         titleView?.updateTitleImage(reverse: true)
         serversView = ServersListView.showIn(view, frame: frameForDropDownOverlay)
+        UIAccessibility.post(notification: UIAccessibility.Notification.screenChanged, argument: serversView?.labelTitle)
         serversView?.delegate = self
     }
 
