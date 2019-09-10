52309d8a4a68a85e29a6cff12954746a5280de2c
cardoso <matheus@cardo.so>
2019-09-10 20:39:45 -0300

    Fix info request parsing wrong format

--- Rocket.ChatTests/API/Requests/General/InfoRequestSpec.swift
+++ Rocket.ChatTests/API/Requests/General/InfoRequestSpec.swift
@@ -23,8 +23,24 @@ class InfoRequestSpec: APITestCase {
     func testProperties() {
         let jsonString = """
         {
-            "version": "1.2.3-develop",
-            "success": true
+          "success": true,
+          "info": {
+            "version": "0.47.0-develop",
+            "build": {
+              "nodeVersion": "v4.6.2",
+              "arch": "x64",
+              "platform": "linux",
+              "cpus": 4
+            },
+            "commit": {
+              "hash": "5901cc7270e3587101631ee222def950d705c611",
+              "date": "Thu Dec 1 19:08:01 2016 -0200",
+              "author": "Gabriel Engel",
+              "subject": "Merge branch 'develop' into experimental",
+              "tag": "0.46.0",
+              "branch": "experimental"
+            }
+          }
         }
         """
 
